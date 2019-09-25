ad2572f31802111312c5a784c225d6a62c252ff9
Ryan Lieu <github-benzBrake@woai.ru>
2019-09-25 17:14:35 +0800

    修正另存为

--- userChromeJS/downloadPlus/downloadPlus.uc.js
+++ userChromeJS/downloadPlus/downloadPlus.uc.js
@@ -1,28 +1,40 @@
 // ==UserScript==
 // @name		downloadPlus.uc.js
 // @description	下载窗口添加:另存为、双击复制链接、第三方工具下载
+// @include		main
 // @include		chrome://mozapps/content/downloads/unknownContentType.xul
-// @version		2019.09.18
+// @include		chrome://browser/content/places/places.xul
+// @version		2019.09.25
 // @startup		window.MDownloadPlus.init();
-// @note		新增链接类型不支持提示，新增第三方应用调用参数 by Ryan Lieu<github-benzBrake@woai.ru>
+// @startup		window.removeDownloadfile.init();
+// @note		修正另存为，新增链接类型不支持提示，新增第三方应用调用参数 by Ryan Lieu<github-benzBrake@woai.ru>
 // @note		适配Firefox57+
 // ==/UserScript==
-
-(location == "chrome://mozapps/content/downloads/unknownContentType.xul") &&
 (function () {
 	const PREF_BD_USEDOWNLOADDIR = "browser.download.useDownloadDir";
 
 	let config = {
 		defaultActionToSave:true,//默认选择下载文件
-		addSaveAsButton:false,//添加另存为按钮，只在选择了默认保存位置时添加（此功能FF68+）无效
+		addSaveAsButton:true, //添加另存为按钮，只在选择了默认保存位置时添加（此功能FF68+）无效
 		copySourceByDbClick:true,//来源显示完整目录并支持双击复制完整地址
 		useExtraAppDownload:true,//使用第三方下载工具下载
-		extraAppName:"IDM",//下载工具名称
-		extraAppPath:"C:\\Program\ Files\ (x86)\\Internet\ Download\ Manager\\IDMan.exe", //下载工具路径
-		extraAppParameter: ["/d"],//下载工具参数
+		/* 迅雷示例 */
+		extraAppName:"迅雷",//下载工具名称
+		extraAppPath:"C:\\Program\ Files\ (x86)\\Thunder\ Network\\Program\\Thunder.exe", //下载工具路径
+		extraAppParameter: [],//下载工具参数
+		/* IDM */
+		//extraAppName:"IDM",//下载工具名称
+		//extraAppPath:"C:\\Program\ Files\ (x86)\\Internet\ Download\ Manager\\IDMan.exe", //下载工具路径
+		//extraAppParameter: ["/d"],//下载工具参数
 		_:false
 	};
-
+	
+	function isUsableDirectory(aDirectory)
+	{
+		return aDirectory.exists() && aDirectory.isDirectory() &&
+			aDirectory.isWritable();
+	}
+	
 	var downloadModule = {};
 	Components.utils.import("resource://gre/modules/DownloadLastDir.jsm", downloadModule);
 	Components.utils.import("resource://gre/modules/Downloads.jsm");
@@ -47,7 +59,7 @@
 			let extraApp = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
 			extraApp.initWithPath(config.extraAppPath);
 			if (!extraApp.exists()) {
-				alert(config.extraAppName+ "不存在: " + config.extraAppPath);
+				alert("找不到" + config.extraAppName + "，请点击“火箭图标->自定义设置->下载设置”配置下载工具！");
 				return;
 			}
 			try {
@@ -63,7 +75,7 @@
 			dialog.mDialog.dialog = null;
 			window.close();
 		},
-		createSaveAsButton(){
+		createSaveAsButton:function (){
 			let prefs = Components.classes["@mozilla.org/preferences-service;1"]
 				.getService(Components.interfaces.nsIPrefBranch);
 			let autodownload = prefs.getBoolPref(PREF_BD_USEDOWNLOADDIR, false);
@@ -77,87 +89,9 @@
 			}
 		},
 		saveAs:function () {
-			var modeGroup = dialog.dialogElement("mode");
-			modeGroup.selectedItem = dialog.dialogElement("save");
-			let aLauncher = dialog.mLauncher;
-			let aContext = dialog.mContext;
-			(async () => {
-				var nsIFilePicker = Components.interfaces.nsIFilePicker;
-				var picker = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-
-				var parent,gDownloadLastDir;
-				try {
-					parent = aContext.QueryInterface(Components.interfaces.nsIInterfaceRequestor).getInterface(Components.interfaces.nsIDOMWindow);
-				} catch (ex) {}
-
-				if (parent) {
-					gDownloadLastDir = new downloadModule.DownloadLastDir(parent);
-				} else {
-					gDownloadLastDir = this._mDownloadDir;
-					let windowsEnum = Services.wm.getEnumerator("");
-					while (windowsEnum.hasMoreElements()) {
-						let someWin = windowsEnum.getNext();
-						if (someWin != this.mDialog) {
-							parent = someWin;
-						}
-					}
-					if (!parent) {
-						Components.utils.reportError("No candidate parent windows were found for the save filepicker." +
-							"This should never happen.");
-					}
-				}
-
-				picker.init(parent, "另存为", nsIFilePicker.modeSave);
-				picker.defaultString = aLauncher.suggestedFileName;
-				try {
-					picker.defaultExtension = this.mLauncher.MIMEInfo.primaryExtension;
-				}
-				catch (ex) { }
-
-				picker.appendFilters( nsIFilePicker.filterAll );
-
-				let preferredDir = await Downloads.getPreferredDownloadsDirectory();
-				picker.displayDirectory = new FileUtils.File(preferredDir);
-
-				gDownloadLastDir.getFileAsync(aLauncher.source, lastDir => {
-					if (lastDir && isUsableDirectory(lastDir))
-						picker.displayDirectory = lastDir;
-
-					picker.open(returnValue => {
-						if (returnValue == nsIFilePicker.returnCancel) {
-							aLauncher.saveDestinationAvailable(null);
-							return;
-						}
-						result = picker.file;
-
-						if (result) {
-							try {
-								if (result.exists() && dialog.getFinalLeafName(result.leafName) == result.leafName)
-									result.remove(false);
-							}
-							catch (ex) {
-							}
-							var newDir = result.parent.QueryInterface(Components.interfaces.nsIFile);
-							gDownloadLastDir.setFile(aLauncher.source, newDir);
-
-							try {
-								result = dialog.validateLeafName(newDir, result.leafName, null);
-							}
-							catch (ex) {
-								if (ex.result == Components.results.NS_ERROR_FILE_AComponents.classesESS_DENIED) {
-									dialog.displayBadPermissionAlert();
-									aLauncher.saveDestinationAvailable(null);
-									return;
-								}
-							}
-						}
-						aLauncher.saveDestinationAvailable(result);
-						dialog.mDialog.dialog = null;
-						window.close();
-
-					});
-				});
-			})().catch(Components.utils.reportError);
+			var mainWindow = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+			mainWindow.eval("(" + mainWindow.internalSave.toString().replace("let ", "").replace("var fpParams", "fileInfo.fileExt=null;fileInfo.fileName=aDefaultFileName;var fpParams") + ")")(dialog.mLauncher.source.asciiSpec, null, (document.querySelector("#locationtext") ? document.querySelector("#locationtext").value : dialog.mLauncher.suggestedFileName), null, null, null, null, null, null, mainWindow.document, 0, null);
+			close();
 		},
 		copySourceByDbClick:function () {
 			var source = document.querySelector("#source");
@@ -167,7 +101,6 @@
 			source.setAttribute("ondblclick", 'Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(dialog.mLauncher.source.spec)');
 		},
 		init:function () {
-
 			if(config.defaultActionToSave){
 				var modeGroup = dialog.dialogElement("mode");
 				modeGroup.selectedItem = dialog.dialogElement("save");
@@ -183,16 +116,14 @@
 
 			if(config.useExtraAppDownload){
 				this.createExtraAppButton();
+				this.createIDMAppButton();
 			}
 		}
 	}
 
-	function isUsableDirectory(aDirectory)
-	{
-		return aDirectory.exists() && aDirectory.isDirectory() &&
-			aDirectory.isWritable();
+	if (location.href.startsWith("chrome://mozapps/content/downloads/unknownContentType.x")) {
+		MDownloadPlus.init();
+		window.MDownloadPlus = MDownloadPlus;
+		window.sizeToContent();
 	}
-
-	MDownloadPlus.init();
-	window.MDownloadPlus = MDownloadPlus;
 })()
