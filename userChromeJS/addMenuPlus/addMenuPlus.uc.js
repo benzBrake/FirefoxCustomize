20db9976b25ac7427928cb294ca3ca0149168760
Ryan Lieu <github-benzBrake@woai.ru>
2022-05-05 20:09:25 +0800

    docs: addMenuPlus.uc.js 修改注释

--- userChromeJS/addMenuPlus/addMenuPlus.uc.js
+++ userChromeJS/addMenuPlus/addMenuPlus.uc.js
@@ -16,7 +16,7 @@
 // @reviewURL      http://bbs.kafan.cn/thread-1554431-1-1.html
 // @downloadURL    https://github.com/ywzhaiqi/userChromeJS/raw/master/addmenuPlus/addMenuPlus.uc.js
 // @note           0.1.3 还原不知道被谁删掉的配置文件路径配置项，修复 openCommand bug，修复 exec 目录读取图标报错
-// @note           0.1.2 修复 %I %IMAGE_URL% %IMAGE_BASE64% 转换为空白字符串 this.t is not function，GroupMenu 增加 onshowing 事件
+// @note           0.1.2 增加多语言，修复 %I %IMAGE_URL% %IMAGE_BASE64% 转换为空白字符串 this.t is not function，GroupMenu 增加 onshowing 事件
 // @note           0.1.1 Places keywords API を使うようにした
 // @note           0.1.0 menugroup をとりあえず利用できるようにした
 // @note           0.0.9 Firefox 29 の Firefox Button 廃止に伴いファイルメニューに追加するように変更
