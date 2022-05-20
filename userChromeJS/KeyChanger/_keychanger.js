095a5ef90bd2dde65d231aafb7ed9be67f3c3da1
Ryan Lieu <github-benzBrake@woai.ru>
2022-05-20 11:51:31 +0800

    fix: KeyChanger 配置 BUG 1. 有时候 F3 无法查找下一个 2. 有时候 Alt + I 无法弹出网页信息窗口 3. 谷歌/百度站内搜索无法搜索选中文本

--- userChromeJS/KeyChanger/_keychanger.js
+++ userChromeJS/KeyChanger/_keychanger.js
@@ -7,8 +7,7 @@
 keys['F1'] = function () {
     document.getElementById("cmd_newNavigatorTab").doCommand();
 }; // 新建标签页并光标定位到地址栏
-keys['F2'] = function (event) {
-
+keys['F2'] = function () {
     let tabs = gBrowser.tabs, i;
     for (i = 0; i < tabs.length; i++) {
         if (tabs[i] == gBrowser.selectedTab) break;
@@ -16,18 +15,19 @@ keys['F2'] = function (event) {
     i = (i - 1 + tabs.length) % tabs.length;
     gBrowser.selectTabAtIndex(i);
 }; // 上一个标签页
-keys['F3'] = function (event) {
-    let findBar = document.querySelector('findbar');
-    if (!findBar || findBar.hidden === true) {
-        let tabs = gBrowser.tabs, i;
-        for (i = 0; i < tabs.length; i++) {
-            if (tabs[i] == gBrowser.selectedTab) break;
+keys['F3'] = function () {
+    gBrowser.getFindBar().then(findBar => {
+        if (findBar.getAttribute('hidden')) {
+            let tabs = gBrowser.tabs, i;
+            for (i = 0; i < tabs.length; i++) {
+                if (tabs[i] == gBrowser.selectedTab) break;
+            }
+            i = (i + 1) % tabs.length;
+            gBrowser.selectTabAtIndex(i);
+        } else {
+            document.querySelector('.findbar-find-next').click();
         }
-        i = (i + 1) % tabs.length;
-        gBrowser.selectTabAtIndex(i);
-    } else {
-        findBar.querySelector('.findbar-find-next').click();
-    }
+    });
 }; // 下一个标签页
 keys['F4'] = "duplicateTabIn(gBrowser.selectedTab, 'tab')"; //复制当前标签页
 //keys['F5'] =""; // 原生功能：刷新
@@ -39,7 +39,7 @@ keys['F8'] = function () {
     });
 }; //编辑当前页面
 //keys['F9'] = function() {};// 原生功能：进入阅读模式
-keys['F10'] = function (event) {
+keys['F10'] = function () {
     gBrowser.loadURI("javascript:(function(bookmarklets)%7Bfor(var%20i=0;i%3Cbookmarklets.length;i++)%7Bvar%20code=bookmarklets%5Bi%5D.url;if(code.indexOf(%22javascript:%22)!=-1)%7Bcode=code.replace(%22javascript:%22,%22%22);eval(code)%7Delse%7Bcode=code.replace(/%5Es+%7Cs+$/g,%22%22);if(code.length%3E0)%7Bwindow.open(code)%7D%7D%7D%7D)(%5B%7Btitle:%22%E7%A0%B4%E9%99%A4%E5%8F%B3%E9%94%AE%E8%8F%9C%E5%8D%95%E9%99%90%E5%88%B6%22,url:%22javascript:function%20applyWin(a)%7Bif(typeof%20a.__nnANTImm__===%5Cx22undefined%5Cx22)%7Ba.__nnANTImm__=%7B%7D;a.__nnANTImm__.evts=%5B%5Cx22mousedown%5Cx22,%5Cx22mousemove%5Cx22,%5Cx22copy%5Cx22,%5Cx22contextmenu%5Cx22%5D;a.__nnANTImm__.initANTI=function()%7Ba.__nnantiflag__=true;a.__nnANTImm__.evts.forEach(function(c,b,d)%7Ba.addEventListener(c,this.fnANTI,true)%7D,a.__nnANTImm__)%7D;a.__nnANTImm__.clearANTI=function()%7Bdelete%20a.__nnantiflag__;a.__nnANTImm__.evts.forEach(function(c,b,d)%7Ba.removeEventListener(c,this.fnANTI,true)%7D,a.__nnANTImm__);delete%20a.__nnANTImm__%7D;a.__nnANTImm__.fnANTI=function(b)%7Bb.stopPropagation();return%20true%7D;a.addEventListener(%5Cx22unload%5Cx22,function(b)%7Ba.removeEventListener(%5Cx22unload%5Cx22,arguments.callee,false);if(a.__nnantiflag__===true)%7Ba.__nnANTImm__.clearANTI()%7D%7D,false)%7Da.__nnantiflag__===true?a.__nnANTImm__.clearANTI():a.__nnANTImm__.initANTI()%7DapplyWin(top);var%20fs=top.document.querySelectorAll(%5Cx22frame,%20iframe%5Cx22);for(var%20i=0,len=fs.length;i%3Clen;i++)%7Bvar%20win=fs%5Bi%5D.contentWindow;try%7Bwin.document%7Dcatch(ex)%7Bcontinue%7DapplyWin(fs%5Bi%5D.contentWindow)%7D;void%200;%22%7D,%7Btitle:%22%E7%A0%B4%E9%99%A4%E9%80%89%E6%8B%A9%E5%A4%8D%E5%88%B6%E9%99%90%E5%88%B6%22,url:%22javascript:(function()%7Bvar%20doc=document;var%20bd=doc.body;bd.onselectstart=bd.oncopy=bd.onpaste=bd.onkeydown=bd.oncontextmenu=bd.onmousemove=bd.onselectstart=bd.ondragstart=doc.onselectstart=doc.oncopy=doc.onpaste=doc.onkeydown=doc.oncontextmenu=null;doc.onselectstart=doc.oncontextmenu=doc.onmousedown=doc.onkeydown=function%20()%7Breturn%20true;%7D;with(document.wrappedJSObject%7C%7Cdocument)%7Bonmouseup=null;onmousedown=null;oncontextmenu=null;%7Dvar%20arAllElements=document.getElementsByTagName(%5Cx27*%5Cx27);for(var%20i=arAllElements.length-1;i%3E=0;i--)%7Bvar%20elmOne=arAllElements;with(elmOne.wrappedJSObject%7C%7CelmOne)%7Bonmouseup=null;onmousedown=null;%7D%7Dvar%20head=document.getElementsByTagName(%5Cx27head%5Cx27)%5B0%5D;if(head)%7Bvar%20style=document.createElement(%5Cx27style%5Cx27);style.type=%5Cx27text/css%5Cx27;style.innerHTML=%5Cx22html,*%7B-moz-user-select:auto!important;%7D%5Cx22;head.appendChild(style);%7Dvoid(0);%7D)();%22%7D%5D)", {
         triggeringPrincipal: gBrowser.contentPrincipal
     });
@@ -71,18 +71,18 @@ keys["Alt+F3"] = function () {
     gBrowser.removeAllTabsBut(gBrowser.selectedTab);
 }; //关闭其他标签页
 
-keys['Alt+F'] = function (event) {
+keys['Alt+F'] = function () {
     var bar = document.getElementById("PersonalToolbar");
     setToolbarVisibility(bar, bar.collapsed);
 }; //显示或隐藏书签栏， 自带按键 Ctrl+Shift+B
-keys['Alt+G'] = function (event) {
-    var sel = (gContextMenu || { textSelected: "" }).textSelected;
-    if (sel.length == 0) sel = prompt('谷歌站内搜索:', '');
-    if (sel.length > 0) gBrowser.addTrustedTab('https://www.google.com/search?q=site:' + encodeURIComponent(gBrowser.currentURI.host) + ' ' + encodeURIComponent(sel));
+keys['Alt+G'] = function () {
+    KeyChanger.openCommand("javascript:var%20Bar=location.host+%22%22;q%20=%20%22%22%20+%20(window.getSelection%20?%20window.getSelection()%20:%20document.getSelection%20?%20document.getSelection()%20:%20document.selection.createRange().text);%20if%20(!q)%20q%20=%20prompt(%22\u8BF7\u8F93\u5165\u641C\u7D22\u7684\u5173\u952E\u8BCD:%22,%20%22%22);%20if%20(q!=null)%20{var%20qlocation=%22%20%22;qlocation=('http://www.google.com/search?num=30&hl=zh-CN&newwindow=1&q='+q+'&sitesearch='+Bar+'');window.open(qlocation);}%20void%200");
 }; //Google站内搜索
-keys['Alt+B'] = "var s = prompt('百度站内搜索:', '');if (s.length > 0) gBrowser.addTrustedTab('https://www.baidu.com/s?wd=site:' + encodeURIComponent( gBrowser.currentURI.host) + ' ' + encodeURIComponent(s));"; //Baidu站内搜索
-keys['Alt+I'] = function (ev) {
-    BrowserPageInfo();
+keys['Alt+B'] = function () {
+    KeyChanger.openCommand("javascript:var%20Bar=location.host+%22%22;q%20=%20%22%22%20+%20(window.getSelection%20?%20window.getSelection()%20:%20document.getSelection%20?%20document.getSelection()%20:%20document.selection.createRange().text);%20if%20(!q)%20q%20=%20prompt(%22%5Cu8BF7%5Cu8F93%5Cu5165%5Cu641C%5Cu7D22%5Cu7684%5Cu5173%5Cu952E%5Cu8BCD:%22,%20%22%22);%20if%20(q!=null)%20%7Bvar%20qlocation=%22%20%22;qlocation=('http://www.baidu.com/s?&ie=UTF-8&oe=UTF-8&cl=3&rn=100&wd=%20%20'+q+'%20%20%20site:%20'+Bar+'');window.open(qlocation);%7D%20void%200");
+};//Baidu站内搜索
+keys['Alt+I'] = function () {
+    event.target.ownerGlobal.BrowserPageInfo();
 }; //查看页面信息
 keys['Alt+Z'] = function () {
     try {
