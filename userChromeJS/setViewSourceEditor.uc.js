26d79a276e6e4fa36dd2dc28c044c1ba34770bbd
Ryan Lieu <github-benzBrake@woai.ru>
2022-05-25 11:46:27 +0800

    feat: 上传 Nodepad 2

--- userChromeJS/setViewSourceEditor.uc.js
+++ userChromeJS/setViewSourceEditor.uc.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name		设置默认代码编辑器
-// @description	设置默认代码编辑器为 Notepad2
-// @filename	setViewSourceEditor.uc.js
-// @author		Ryan
-// @shutdown	UC.setViewSourceEditor.destroy();
-// @homepageURL	https://github.com/benzBrake/FirefoxCustomize
+// @name        setViewSourceEditor.uc.js
+// @description 设置默认编辑器为Notepad2，仅兼容 xiaoxiaoflood 的 userChromeJS 环境
+// @author      Ryan
+// @shutdown    UC.setViewSourceEditor.destroy();
+// @homepageURL https://github.com/benzBrake/FirefoxCustomize
 // @onlyonce
 // ==/UserScript==
 
-// 需要把 Notepad2 放在 local 目录下的 Notepad2 文件夹内
+// 需要把 Notepad2 放在 配置目录\chrome\resources\bin\Notepad2 文件夹内
 (function () {
     UC.setViewSourceEditor = {
         init: function () {
-            xPref.set('view_source.editor.path', _uc.chromedir.path + "\\Local\\Notepad2\\Notepad2.exe");
+            let UChrm = Services.dirsvc.get('UChrm', Ci.nsIFile);
+            xPref.set('view_source.editor.path', UChrm.path + "\\resources\\bin\\Notepad2\\Notepad2.exe");
         },
         destroy: function () {
             xPref.clear('view_source.editor.path');
