0da5191f30c5da45d30131c7c4b74092623087ac
Ryan Lieu <github-benzBrake@woai.ru>
2022-07-01 23:41:25 +0800

    fix: (JS) setPersonalToolbarVisibility.uc.js 去除无用输出和增加说明

--- userChromeJS/setPersonalToolbarVisibility.uc.js
+++ userChromeJS/setPersonalToolbarVisibility.uc.js
@@ -5,6 +5,7 @@
 // @charset         UTF-8
 // @include         chrome://browser/content/browser.xhtml
 // @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @note            需要把书签工具栏模式设置为在新标签页显示
 // ==/UserScript==
 (function () {
     const URLS = [
@@ -12,11 +13,10 @@
         'about:blank',
         'about:privatebrowsing',
         'about:home'
-    ]
+    ];
     setTimeout(function () {
         let event = (event) => {
             const win = event.target.ownerGlobal;
-            win.console.log(event.type);
             const bar = win.document.getElementById("PersonalToolbar");
             win.setToolbarVisibility(bar, URLS.contain(win.gBrowser.currentURI.spec));
         }
