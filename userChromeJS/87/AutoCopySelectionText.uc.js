2de57d6532db6df5383667ba1632b0cf9240ac84
Ryan Lieu <github-benzBrake@woai.ru>
2022-07-16 23:21:49 +0800

    feat: (JS) AutoCopySelectionText.uc.js 重写代码，支持内置页面，支持热插拔

--- userChromeJS/87/AutoCopySelectionText.uc.js
+++ userChromeJS/87/AutoCopySelectionText.uc.js
@@ -1,27 +1,85 @@
-
 // ==UserScript==
 // @name            AutoCopySelectionText.uc.js
 // @description     自动复制选中文本（ScrLk 亮起时不复制）
 // @author          Ryan
-// @version         2022.07.13
+// @version         2022.07.16
 // @compatibility   Firefox 87
-// @include         main
+// @charset         UTF-8
 // @system          windows
-// @homepageURL     https://github.com/benzBrake/FirefoxCustomize
+// @license         MIT License
+// @include         main
+// @shutdown        window.AutoCopySelectionText.destroy();
+// @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @version         2022.07.16 重写代码，支持热插拔，采用 异步消息，支持 Firefox 内置页面
+// @version         2022.07.13 初始化版本
 // ==/UserScript==
-location.href.startsWith('chrome://browser/content/browser.x') && setTimeout(() => {
-    Components.utils.import("resource://gre/modules/ctypes.jsm");
-    var lib = ctypes.open("user32.dll");
-    const getKeyState = lib.declare('GetKeyState', ctypes.winapi_abi, ctypes.bool, ctypes.int);
-    gBrowser.tabpanels.addEventListener("mouseup", function () {
-        try {
-            if (getKeyState(0x91)) return;
-            // 0x91 是 ScrLk 的按键码，ScrLk 灯亮起时不复制
-            gBrowser.selectedBrowser.finder.getInitialSelection().then((r) => {
-                if (r.selectedText) {
-                    Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(r.selectedText);
+(function () {
+    class AutoCopySelectionText {
+        constructor() {
+            Components.utils.import("resource://gre/modules/ctypes.jsm");
+            let user32 = ctypes.open("user32.dll");
+            this.getKeyState = user32.declare('GetKeyState', ctypes.winapi_abi, ctypes.bool, ctypes.int);
+            function frameScript() {
+                content.acst_Content = {
+                    init: function () {
+                        addMessageListener("acst_getSelectedText", this);
+                        addMessageListener("acst_destroy", this);
+                    },
+                    receiveMessage: function (message) {
+                        switch (message.name) {
+                            case 'acst_getSelectedText':
+                                let sel = content.getSelection();
+                                let data = { text: sel.toString() }
+                                sendSyncMessage("acst_selectionData", data);
+                                break;
+                            case 'acst_destroy':
+                                this.destroy();
+                                break;
+                        }
+                    },
+                    destroy() {
+                        removeMessageListener("acst_getSelectedText", this);
+                        removeMessageListener("acst_destroy", this);
+                        delete content.acst_Content;
+                    }
                 }
-            })
-        } catch (e) { }
-    }, false);
-}, 1000);
+                content.acst_Content.init();
+            }
+            let frameScriptURI = 'data:application/javascript,'
+                + encodeURIComponent('(' + frameScript.toString() + ')()');
+            window.messageManager.loadFrameScript(frameScriptURI, true);
+            window.messageManager.addMessageListener("acst_selectionData", this);
+
+            gBrowser.tabpanels.addEventListener("mouseup", this, false);
+        }
+        trigger(browser) {
+            browser || (browser = gBrowser.selectedTab.linkedBrowser);
+            browser.messageManager.sendAsyncMessage("acst_getSelectedText");
+        }
+        receiveMessage(message) {
+            switch (message.name) {
+                case 'acst_selectionData':
+                    if (message.data.text)
+                        Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(message.data.text);
+                    break;
+            }
+        }
+        handleEvent(event) {
+            switch (event.type) {
+                case 'mouseup':
+                    // 鼠标按键释放时读取选中文本
+                    if (this.getKeyState(0x91)) return;
+                    try {
+                        gBrowser.selectedTab.linkedBrowser.messageManager.sendAsyncMessage("acst_getSelectedText");
+                    } catch (e) { }
+            }
+        }
+        destroy() {
+            window.messageManager.broadcastAsyncMessage("acst_destroy");
+            window.messageManager.removeMessageListener("acst_selectionData", this);
+            delete window.AutoCopySelectionText;
+        }
+    }
+
+    window.AutoCopySelectionText = new AutoCopySelectionText();
+})()
