6ca4344ad19c01ac508608a964721921cccb9bbf
Ryan Lieu <github-benzBrake@woai.ru>
2022-07-18 16:16:41 +0800

    feat: (JS) AutoCopySelectionText.uc.js 增加延时参数 #1

--- userChromeJS/87/AutoCopySelectionText.uc.js
+++ userChromeJS/87/AutoCopySelectionText.uc.js
@@ -2,7 +2,7 @@
 // @name            AutoCopySelectionText.uc.js
 // @description     自动复制选中文本（ScrLk 亮起时不复制）
 // @author          Ryan
-// @version         2022.07.16
+// @version         2022.07.18
 // @compatibility   Firefox 87
 // @charset         UTF-8
 // @system          windows
@@ -10,6 +10,7 @@
 // @include         main
 // @shutdown        window.AutoCopySelectionText.destroy();
 // @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @version         2022.07.18 支持长按延时
 // @version         2022.07.16 重写代码，支持热插拔，采用 异步消息，支持 Firefox 内置页面
 // @version         2022.07.13 初始化版本
 // ==/UserScript==
@@ -19,6 +20,9 @@
             Components.utils.import("resource://gre/modules/ctypes.jsm");
             let user32 = ctypes.open("user32.dll");
             this.getKeyState = user32.declare('GetKeyState', ctypes.winapi_abi, ctypes.bool, ctypes.int);
+            this.TIMEOUT_ID = null;
+            this.WAIT_TIME = 0; // Change it to any number as you want
+            this.LONG_PRESS = false;
             function frameScript() {
                 content.acst_Content = {
                     init: function () {
@@ -33,7 +37,7 @@
                                 sendSyncMessage("acst_selectionData", data);
                                 break;
                             case 'acst_destroy':
-                                this.destroy();
+                                content.acst_Content.destroy();
                                 break;
                         }
                     },
@@ -49,8 +53,7 @@
                 + encodeURIComponent('(' + frameScript.toString() + ')()');
             window.messageManager.loadFrameScript(frameScriptURI, true);
             window.messageManager.addMessageListener("acst_selectionData", this);
-
-            gBrowser.tabpanels.addEventListener("mouseup", this, false);
+            ["mousemove", "mouseup"].forEach((t) => gBrowser.tabpanels.addEventListener(t, this, false))
         }
         trigger(browser) {
             browser || (browser = gBrowser.selectedTab.linkedBrowser);
@@ -65,18 +68,29 @@
             }
         }
         handleEvent(event) {
+            if (this.TIMEOUT_ID)
+                clearTimeout(this.TIMEOUT_ID);
+            if (this.getKeyState(0x91)) return;
             switch (event.type) {
+                case 'mousemove':
+                    this.TIMEOUT_ID = setTimeout(function () {
+                        window.AutoCopySelectionText.LONG_PRESS = true;
+                    }, this.WAIT_TIME)
                 case 'mouseup':
                     // 鼠标按键释放时读取选中文本
-                    if (this.getKeyState(0x91)) return;
-                    try {
-                        gBrowser.selectedTab.linkedBrowser.messageManager.sendAsyncMessage("acst_getSelectedText");
-                    } catch (e) { }
+                    if (window.AutoCopySelectionText.LONG_PRESS) {
+                        try {
+                            gBrowser.selectedTab.linkedBrowser.messageManager.sendAsyncMessage("acst_getSelectedText");
+                        } catch (e) { }
+                    }
+                    break;
             }
+            this.LONG_PRESS = false;
         }
         destroy() {
             window.messageManager.broadcastAsyncMessage("acst_destroy");
             window.messageManager.removeMessageListener("acst_selectionData", this);
+            ["mousemove", "mouseup"].forEach((t) => gBrowser.tabpanels.removeEventListener(t, this, false))
             delete window.AutoCopySelectionText;
         }
     }
