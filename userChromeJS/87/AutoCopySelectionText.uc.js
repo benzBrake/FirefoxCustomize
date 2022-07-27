9183b169b7860e5b2aed7ffd60d052554210347d
Ryan Lieu <github-benzBrake@woai.ru>
2022-07-27 18:32:50 +0800

    feat: (JS) AutoCopySelectionText.uc.js 支持网页文本框选中复制

--- userChromeJS/87/AutoCopySelectionText.uc.js
+++ userChromeJS/87/AutoCopySelectionText.uc.js
@@ -2,7 +2,7 @@
 // @name            AutoCopySelectionText.uc.js
 // @description     自动复制选中文本（ScrLk 亮起时不复制）
 // @author          Ryan
-// @version         2022.07.18
+// @version         2022.07.28
 // @compatibility   Firefox 87
 // @charset         UTF-8
 // @system          windows
@@ -10,6 +10,7 @@
 // @include         main
 // @shutdown        window.AutoCopySelectionText.destroy();
 // @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @version         2022.07.28 网页支持文本框
 // @version         2022.07.18 支持长按延时
 // @version         2022.07.16 重写代码，支持热插拔，采用 异步消息，支持 Firefox 内置页面
 // @version         2022.07.13 初始化版本
@@ -18,79 +19,101 @@
     class AutoCopySelectionText {
         constructor() {
             Components.utils.import("resource://gre/modules/ctypes.jsm");
+            // will be transfered to control by toolbar button
             let user32 = ctypes.open("user32.dll");
             this.getKeyState = user32.declare('GetKeyState', ctypes.winapi_abi, ctypes.bool, ctypes.int);
-            this.TIMEOUT_ID = null;
-            this.WAIT_TIME = 0; // Change it to any number as you want
-            this.LONG_PRESS = false;
             function frameScript() {
-                content.acst_Content = {
-                    init: function () {
-                        addMessageListener("acst_getSelectedText", this);
-                        addMessageListener("acst_destroy", this);
-                    },
-                    receiveMessage: function (message) {
-                        switch (message.name) {
-                            case 'acst_getSelectedText':
-                                let sel = content.getSelection();
-                                let data = { text: sel.toString() }
+                const { Services } = Components.utils.import(
+                    "resource://gre/modules/Services.jsm"
+                );
+
+                // implement read from about:config preferences in future
+                var WAIT_TIME = 0; // Change it to any number as you want
+                var TRIM_SELECTION = true; // remove spaces before and after the string
+
+                // Do not modify below ------------------------------------------
+                var LONG_PRESS = false;
+                var TIMEOUT_ID = null;
+                function handleEvent(event) {
+                    if (event.button !== 0) return; // only trigger when left button up
+                    if (TIMEOUT_ID)
+                        content.clearTimeout(TIMEOUT_ID);
+                    const focusedElement =
+                        Services.focus.focusedElement ||
+                        event.originalTarget.ownerDocument?.activeElement;
+
+                    switch (event.type) {
+                        case 'mousemove':
+                            TIMEOUT_ID = content.setTimeout(function () {
+                                LONG_PRESS = true;
+                            }, WAIT_TIME);
+                        case 'mouseup':
+                            // copy text on mouse button up
+                            if (LONG_PRESS) {
+                                let data = { text: getSelection(content, focusedElement) }
                                 sendSyncMessage("acst_selectionData", data);
-                                break;
-                            case 'acst_destroy':
-                                content.acst_Content.destroy();
-                                break;
+                            }
+                            break;
+                    }
+                    LONG_PRESS = false;
+                }
+
+                // From addMenuPlus.uc.js
+                function getSelection(win, focusedElement) {
+                    win || (win = content);
+                    var selection = win.getSelection().toString();
+                    if (!selection) {
+                        let element = focusedElement;
+                        let isOnTextInput = function (elem) {
+                            return elem instanceof HTMLTextAreaElement ||
+                                (elem instanceof HTMLInputElement && elem.mozIsTextField(true));
+                        };
+
+                        if (isOnTextInput(element)) {
+                            selection = element.value.substring(element.selectionStart,
+                                element.selectionEnd);
                         }
-                    },
-                    destroy() {
-                        removeMessageListener("acst_getSelectedText", this);
-                        removeMessageListener("acst_destroy", this);
-                        delete content.acst_Content;
+                    }
+
+                    if (TRIM_SELECTION && selection) {
+                        selection = selection.replace(/^\s+/, "")
+                            .replace(/\s+$/, "")
+                            .replace(/\s+/g, " ");
+                    }
+                    return selection;
+                }
+
+                ["mousemove", "mouseup"].forEach((t) => addEventListener(t, handleEvent, false));
+
+                function receiveMessage(message) {
+                    switch (message.name) {
+                        case 'acst_destroy':
+                            ["mousemove", "mouseup"].forEach((t) => removeEventListener(t, handleEvent, false));
+                            removeMessageListener("acst_destroy", receiveMessage);
+                            handleEvent = null;
+                            receiveMessage = null;
+                            break;
                     }
                 }
-                content.acst_Content.init();
+                addMessageListener("acst_destroy", receiveMessage);
             }
             let frameScriptURI = 'data:application/javascript,'
                 + encodeURIComponent('(' + frameScript.toString() + ')()');
             window.messageManager.loadFrameScript(frameScriptURI, true);
             window.messageManager.addMessageListener("acst_selectionData", this);
-            ["mousemove", "mouseup"].forEach((t) => gBrowser.tabpanels.addEventListener(t, this, false))
-        }
-        trigger(browser) {
-            browser || (browser = gBrowser.selectedTab.linkedBrowser);
-            browser.messageManager.sendAsyncMessage("acst_getSelectedText");
         }
         receiveMessage(message) {
             switch (message.name) {
                 case 'acst_selectionData':
+                    if (this.getKeyState(0x91)) return;
                     if (message.data.text)
-                        Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(message.data.text);
+                        Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(message.data.text);
                     break;
             }
-        }
-        handleEvent(event) {
-            if (this.TIMEOUT_ID)
-                clearTimeout(this.TIMEOUT_ID);
-            if (this.getKeyState(0x91)) return;
-            switch (event.type) {
-                case 'mousemove':
-                    this.TIMEOUT_ID = setTimeout(function () {
-                        window.AutoCopySelectionText.LONG_PRESS = true;
-                    }, this.WAIT_TIME)
-                case 'mouseup':
-                    // 鼠标按键释放时读取选中文本
-                    if (window.AutoCopySelectionText.LONG_PRESS) {
-                        try {
-                            gBrowser.selectedTab.linkedBrowser.messageManager.sendAsyncMessage("acst_getSelectedText");
-                        } catch (e) { }
-                    }
-                    break;
-            }
-            this.LONG_PRESS = false;
         }
         destroy() {
             window.messageManager.broadcastAsyncMessage("acst_destroy");
             window.messageManager.removeMessageListener("acst_selectionData", this);
-            ["mousemove", "mouseup"].forEach((t) => gBrowser.tabpanels.removeEventListener(t, this, false))
             delete window.AutoCopySelectionText;
         }
     }
