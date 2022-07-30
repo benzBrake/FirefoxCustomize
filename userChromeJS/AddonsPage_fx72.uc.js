078b9e32c4a24ea610e8111329111d45bcbac629
Ryan Lieu <github-benzBrake@woai.ru>
2022-07-30 14:52:12 +0800

    feat: (JS) AddonsPage_fx72.uc.js alice0775 环境不再依赖 rebuild_userChrome.uc.js (JS) miscMods.uc.js 正确支持非 xiaoxiaoflood UC 环境

--- userChromeJS/AddonsPage_fx72.uc.js
+++ userChromeJS/AddonsPage_fx72.uc.js
@@ -590,9 +590,28 @@
             this.enabled = !val;
             if (window.userChromejs) {
                 userChromejs.chgScriptStat(this.name);
+            } else if (userChrome_js) {
+                var s = xPref.get("userChrome.disable.script", "");
+                var afilename = this.name;
+                if (!userChrome_js.scriptDisable[afilename]) {
+                    s = (s + ',').replace(afilename + ',', '') + afilename + ',';
+                } else {
+                    s = (s + ',').replace(afilename + ',', '');
+                }
+                s = s.replace(/,,/g, ',').replace(/^,/, '');
+                xPref.set("userChrome.disable.script", s);
+                userChrome_js.scriptDisable = restoreState(s.split(','));
             }
 
             AddonManagerPrivate.callAddonListeners(val ? 'onEnabled' : 'onDisabled', this);
+
+            function restoreState(arr) {
+                var disable = [];
+                for (var i = 0, len = arr.length; i < len; i++)
+                    disable[arr[i]] = true;
+                return disable;
+            }
+
         },
         get permissions() {
             // var perms = AddonManager.PERM_CAN_UNINSTALL;
@@ -654,11 +673,6 @@
         },
     };
 
-
-    AM_Helper.init();
-
-    userChromeJSAddon.init();
-
     function $C(doc, tag, attrs, parent, reference) {
         let node;
         if (tag instanceof Node) {
@@ -684,4 +698,100 @@
         return node;
     }
 
+    if (!window.xPref) {
+        window.xPref = {
+            // Retorna o valor da preferência, seja qual for o tipo, mas não
+            // testei com tipos complexos como nsIFile, não sei como detectar
+            // uma preferência assim, na verdade nunca vi uma
+            get: function (prefPath, def = false, valueIfUndefined, setDefault = true) {
+                let sPrefs = def ?
+                    Services.prefs.getDefaultBranch(null) :
+                    Services.prefs;
+
+                try {
+                    switch (sPrefs.getPrefType(prefPath)) {
+                        case 0:
+                            if (valueIfUndefined != undefined)
+                                return this.set(prefPath, valueIfUndefined, setDefault);
+                            else
+                                return undefined;
+                        case 32:
+                            return sPrefs.getStringPref(prefPath);
+                        case 64:
+                            return sPrefs.getIntPref(prefPath);
+                        case 128:
+                            return sPrefs.getBoolPref(prefPath);
+                    }
+                } catch (ex) {
+                    return undefined;
+                }
+                return;
+            },
+
+            set: function (prefPath, value, def = false) {
+                let sPrefs = def ?
+                    Services.prefs.getDefaultBranch(null) :
+                    Services.prefs;
+
+                switch (typeof value) {
+                    case 'string':
+                        return sPrefs.setStringPref(prefPath, value) || value;
+                    case 'number':
+                        return sPrefs.setIntPref(prefPath, value) || value;
+                    case 'boolean':
+                        return sPrefs.setBoolPref(prefPath, value) || value;
+                }
+                return;
+            },
+
+            lock: function (prefPath, value) {
+                let sPrefs = Services.prefs;
+                this.lockedBackupDef[prefPath] = this.get(prefPath, true);
+                if (sPrefs.prefIsLocked(prefPath))
+                    sPrefs.unlockPref(prefPath);
+
+                this.set(prefPath, value, true);
+                sPrefs.lockPref(prefPath);
+            },
+
+            lockedBackupDef: {},
+
+            unlock: function (prefPath) {
+                Services.prefs.unlockPref(prefPath);
+                let bkp = this.lockedBackupDef[prefPath];
+                if (bkp == undefined)
+                    Services.prefs.deleteBranch(prefPath);
+                else
+                    this.set(prefPath, bkp, true);
+            },
+
+            clear: Services.prefs.clearUserPref,
+
+            // Detecta mudanças na preferência e retorna:
+            // return[0]: valor da preferência alterada
+            // return[1]: nome da preferência alterada
+            // Guardar chamada numa var se quiser interrompê-la depois
+            addListener: function (prefPath, trat) {
+                this.observer = function (aSubject, aTopic, prefPath) {
+                    return trat(xPref.get(prefPath), prefPath);
+                }
+
+                Services.prefs.addObserver(prefPath, this.observer);
+                return {
+                    prefPath: prefPath,
+                    observer: this.observer
+                };
+            },
+
+            // Encerra pref observer
+            // Só precisa passar a var definida quando adicionou
+            removeListener: function (obs) {
+                Services.prefs.removeObserver(obs.prefPath, obs.observer);
+            }
+        }
+    }
+
+    AM_Helper.init();
+
+    userChromeJSAddon.init();
 })();
--- userChromeJS/miscMods.uc.js
+++ userChromeJS/miscMods.uc.js
@@ -10,8 +10,6 @@
 (function () {
     if (!location.href.startsWith("chrome://browser/content/browser.x")) return;
 
-    let { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
-
     const WIDGET_ATTRS = {
         "urlbar-input": {
             el: "#paste-and-go",
@@ -21,7 +19,7 @@
         "urlbar-middle-click": {
             el: "#urlbar",
             onclick: function (e) {
-                if (e.button == 1) copy(gBrowser.currentURI.spec);
+                if (e.button == 1) copyText(gBrowser.currentURI.spec);
             },
         },
         "searchbar": {
@@ -84,134 +82,42 @@
                 if (e.button == 1) {
                     e.preventDefault();
                     e.stopPropagation();
-                    let cookiesPath, binPath, savePath, win = e.target.ownerGlobal, uri = win.gBrowser.selectedBrowser.currentURI;
-                    const PREF_BRANCH = "userChrome.navbar.youGetBtn.", PREF_COOKIES = "COOKIESPATH", PREF_BIN = "BINPATH", PREF_SAVE = "SAVEPATH";
-                    const prefs = Services.prefs.getBranch(PREF_BRANCH);
 
-                    // 请在 about:config 中修改 cookies 存储路径 Please change cookies store path in about:config
-                    try {
-                        cookiesPath = prefs.getStringPref(PREF_COOKIES);
-                    } catch (e) {
-                        cookiesPath = "cookies";
-                    }
+                    var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                        .getService(Components.interfaces.nsIPromptService);
 
-                    cookiesPath = handleRelativePath(cookiesPath);
-                    const aCookiesDir = Services.dirsvc.get("UChrm", Ci.nsIFile);
-                    aCookiesDir.appendRelativePath(cookiesPath);
-
-                    if (!aCookiesDir.exists()) aCookiesDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o666);
-
-                    if (!aCookiesDir.isReadable() || !aCookiesDir.isWritable()) {
-                        alert(Services.locale.appLocaleAsBCP47.includes("zh-") ? "Cookies 保存目录不可读写" : "Cookies storage directory cannot be read or written");
+                    var check = { value: false };               // default the checkbox to false
+                    var input = { value: readFromClipboard() || "" };                  // default the edit field to Bob
+                    var result = prompts.prompt(null, "Save Specified Url", "Please Input a URL?", input, null, check);
+                    if (!result)
                         return;
-                    }
-
-                    // 非网页不响应，可以细化为匹配 you-get.exe 支持的网站，我懒得写正则了
-                    if (uri.spec.startsWith('http')) {
-                        binPath = prefs.getStringPref(PREF_BIN, "");
-                        savePath = prefs.getStringPref(PREF_SAVE, "");
-
-                        function setYouGetPath() {
-                            alert(Services.locale.appLocaleAsBCP47.includes("zh-") ? "请先设置 you-get.exe 的路径!!!" : "Please set you-get.exe path first!!!");
-                            if (Services.locale.appLocaleAsBCP47.includes("zh-")) {
-                                openUrl({ 'target': this }, 'https://lussac.lanzoui.com/b00nc5aab', 'tab');
-                            } else {
-                                openUrl({ 'target': this }, 'https://github.com/LussacZheng/you-get.exe/releases', 'tab');
-                            }
-
-                            choosePathAndSave(Services.locale.appLocaleAsBCP47.includes("zh-") ? "设置 you-get.exe 路径" : "Set you-get.exe path", PREF_BRANCH + PREF_BIN, Ci.nsIFilePicker.modeOpen, {
-                                title: Services.locale.appLocaleAsBCP47.includes("zh-") ? "执行文件" : "Executable file",
-                                param: "*.exe"
-                            })
-                        }
-
-                        // 转换成 netscape 格式，抄袭自 cookie_txt 扩展
-                        function formatCookie(co) {
-                            return [
-                                [
-                                    co.isHttpOnly ? '#HttpOnly_' : '',
-                                    co.host
-                                ].join(''),
-                                co.isDomain ? 'TRUE' : 'FALSE',
-                                co.path,
-                                co.isSecure ? 'TRUE' : 'FALSE',
-                                co.expires,
-                                co.name,
-                                co.value + '\n'
-                            ].join('\t');
-                        }
-
-                        // 保存 cookie 并返回路径
-                        function saveCookie(host) {
-                            if (!host) return;
-                            let cookies = Services.cookies.getCookiesFromHost(host, {});
-                            let string = cookies.map(formatCookie).join('');
-
-                            let file = aCookiesDir.clone();
-                            file.append(`${host}.txt`);
-                            if (file.exists()) {
-                                file.remove(0);
-                            }
-                            file.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0o666);
-
-                            // 保存文件，抄袭自 saveUCJS.uc.js
-                            const charset = 'UTF-8';
-                            const fileStream = Components.classes['@mozilla.org/network/file-output-stream;1']
-                                .createInstance(Components.interfaces.nsIFileOutputStream);
-                            fileStream.init(file, 0x02 | 0x08 | 0x20, -1, 0);
-
-                            const converterStream = Components.classes['@mozilla.org/intl/converter-output-stream;1']
-                                .createInstance(Components.interfaces.nsIConverterOutputStream);
-                            converterStream.init(
-                                fileStream,
-                                charset,
-                                string.length,
-                                Components.interfaces.nsIConverterInputStream.DEFAULT_REPLACEMENT_CHARACTER
-                            );
-
-                            converterStream.writeString(string);
-                            converterStream.close();
-                            fileStream.close();
-                            return file.path;
-                        }
-
-
-                        if (!binPath) {
-                            setYouGetPath();
-                            try {
-                                binPath = prefs.getStringPref(PREF_BIN);
-                            } catch (e) {
-                                return;
-                            }
-                        }
-                        if (!savePath) {
-                            choosePathAndSave(Services.locale.appLocaleAsBCP47.includes("zh-") ? "设置视频保存路径" : "Set video save path", PREF_BRANCH + PREF_SAVE)
-                            try {
-                                savePath = prefs.getStringPref(PREF_SAVE);
-                            } catch (e) {
-                                return;
-                            }
-                        }
-                        let youGet = Services.dirsvc.get("UChrm", Ci.nsIFile);
-                        try {
-                            binPath = handleRelativePath(binPath);
-                            youGet.appendRelativePath(binPath);
-                        } catch (e) {
-                            setYouGetPath();
-                            return;
-                        }
-                        let p = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
-
-                        let commandArgs = ['-c', saveCookie(uri.host), '-o', savePath, uri.spec];
-
-                        p.init(youGet);
-                        p.runw(false, commandArgs, commandArgs.length);
-                    }
+                    let cookieJarSettings = gBrowser.selectedBrowser.cookieJarSettings;
+                    //saveURL(aURL, aOriginalURL, aFileName, aFilePickerTitleKey, aShouldBypassCache,
+                    //        aSkipPrompt, aReferrer, aCookieJarSettings,
+                    //        aSourceDocument,
+                    //        aIsContentWindowPrivate,
+                    //        aPrincipal)
+                    saveURL(
+                        url,
+                        null,
+                        null,
+                        null,
+                        true,
+                        false,
+                        null,
+                        cookieJarSettings,
+                        null,
+                        PrivateBrowsingUtils.isWindowPrivate(window),
+                        Services.scriptSecurityManager.createNullPrincipal({})
+                    );
                 } else if (e.button == 2 && !e.shiftKey) {
                     // 右键打开下载历史
                     e.preventDefault();
                     e.stopPropagation();
-                    DownloadsPanel.showDownloadsHistory();
+                    if (typeof ucjs_downloadManager === "undefined")
+                        DownloadsPanel.showDownloadsHistory();
+                    else
+                        ucjs_downloadManager.openDownloadManager(true);
                 }
             },
         },
@@ -243,7 +149,7 @@
     const DELAY_EXEC = {
         "reload styloaix": {
             command: function () {
-                if (UC && UC.styloaix) UC.styloaix.toggleAll({ reload: true });
+                if (typeof UC !== "undefined" && typeof UC.styloaix !== "undefined") UC.styloaix.toggleAll({ reload: true });
             }
         },
         "warn on quit": {
@@ -287,96 +193,34 @@
         }
     }
 
-    function handleRelativePath(path) {
-        if (path) {
-            path = path.replace(/\//g, '\\').toLocaleLowerCase();
-            var ffdir = Cc['@mozilla.org/file/directory_service;1'].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile).path;
-            if (/^(\\)/.test(path)) {
-                return ffdir + path;
-            } else {
-                return path;
-            }
-        }
-    }
-
-    function choosePathAndSave(title, prefKey, mode, filter) {
-        title = title || Services.locale.appLocaleAsBCP47.includes("zh-") ? "选择目录" : "Choose path";
-        mode = mode || Ci.nsIFilePicker.modeGetFolder;
-        let fp = Cc['@mozilla.org/filepicker;1'].createInstance(Ci.nsIFilePicker);
-        fp.init(window, title, mode);
-        if (filter) {
-            let { title, param } = filter;
-            fp.appendFilter(title, param);
-        }
-        fp.open(res => {
-            if (res != Ci.nsIFilePicker.returnOK) return;
-            Services.prefs.setStringPref(prefKey, fp.file.path + '\\');
-        });
-    }
-
-    function openUrl(event, url, where, postData) {
-        var uri;
-        postData = postData || null;
-        try {
-            uri = Services.io.newURI(url, null, null);
-        } catch (e) {
-            return this.log("链接有误：%s".replace("%s", url));
-        }
-        if (uri.scheme === "javascript") {
-            try {
-                loadURI(url);
-            } catch (e) {
-                gBrowser.loadURI(url, { triggeringPrincipal: gBrowser.contentPrincipal });
-            }
-        } else if (where) {
-            openUILinkIn(uri.spec, where, {
-                postData: postData,
-                triggeringPrincipal: where === 'current' ?
-                    gBrowser.selectedBrowser.contentPrincipal : (
-                        /^(f|ht)tps?:/.test(uri.spec) ?
-                            Services.scriptSecurityManager.createNullPrincipal({}) :
-                            Services.scriptSecurityManager.getSystemPrincipal()
-                    )
-            });
-        } else if (event.button == 1) {
-            openUILinkIn(uri.spec, 'tab', {
-                postData: postData,
-                triggeringPrincipal: /^(f|ht)tps?:/.test(uri.spec) ?
-                    Services.scriptSecurityManager.createNullPrincipal({}) :
-                    Services.scriptSecurityManager.getSystemPrincipal()
-            });
-        } else {
-            openUILinkIn(uri.spec, 'tab', {
-                inBackground: false,
-                postData: postData,
-                triggeringPrincipal: Services.scriptSecurityManager.createNullPrincipal({})
-            });
-        }
-    }
-
     function init() {
         for (let widget in WIDGET_ATTRS) {
             let obj = WIDGET_ATTRS[widget];
             try {
-                let { node } = CustomizableUI.getWidget(widget)?.forWindow(window),
-                    { el, initEvent, arg } = obj;
-                arg || (arg = false)
-                let callback = (e) => {
-                    if (e.type == "click" && e.button !== 2) return;
-                    var timer = setInterval(() => {
-                        if (el && $(el)) {
-                            clearInterval(timer);
-                            applyAttrs($(el), obj);
-                            node.removeEventListener(initEvent, callback, arg);
-                        }
-                    }, 10)
-                }
-                node = node || $(el);
-                if (!node) return;
-                if (initEvent) {
-                    node.addEventListener(initEvent, callback, arg);
+                let oWidget = CustomizableUI.getWidget(widget);
+                if (oWidget) {
+                    let { node } = oWidget.forWindow(window),
+                        { el, initEvent, arg } = obj;
+                    arg || (arg = false)
+                    let callback = (e) => {
+                        if (e.type == "click" && e.button !== 2) return;
+                        var timer = setInterval(() => {
+                            if (el && $(el)) {
+                                clearInterval(timer);
+                                applyAttrs($(el), obj);
+                                node.removeEventListener(initEvent, callback, arg);
+                            }
+                        }, 10)
+                    }
+                    node = node || $(el);
+                    if (!node) return;
+                    if (initEvent) {
+                        node.addEventListener(initEvent, callback, arg);
+                    } else {
+                        applyAttrs(node, obj);
+                    }
                 } else {
-                    applyAttrs(node, obj);
+                    Cu.reportError(widget + " is not exists");
                 }
             } catch (e) {
                 Cu.reportError(e)
@@ -392,7 +236,7 @@
         }
     }
 
-    function copy(aText) {
+    function copyText(aText) {
         Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(aText);
     }
 
