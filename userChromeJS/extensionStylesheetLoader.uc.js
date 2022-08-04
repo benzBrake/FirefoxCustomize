1fac6e5a9c4296f56d14eae89803b079bad778d9
Ryan Lieu <github-benzBrake@woai.ru>
2022-08-04 09:58:19 +0800

    fix (JS) extensionStylesheetLoader.uc.js, moveReloadIntoUrl.uc.js fx103

--- userChromeJS/extensionStylesheetLoader.uc.js
+++ userChromeJS/extensionStylesheetLoader.uc.js
@@ -1,25 +1,29 @@
 // ==UserScript==
 // @name           Extension Stylesheet Loader
-// @version        1.0.2
+// @version        1.1.1
 // @author         aminomancer
 // @homepage       https://github.com/aminomancer
-// @description    Allows users to share stylesheets for webextensions without needing to edit the
-// URL. This works by creating an actor in every extension browser that sets an attribute on the
-// root element to expose the addon's ID to user stylesheets. This means we can use the addon's ID
-// instead of @-moz-document url(). That is good because addons' URLs are randomly generated upon
-// install, meaning the URLs I specify in resources/in-content/ext-*.css will not be the same as
-// yours, so they will not work for you. You can also use this in combination with my
-// debugExtensionInToolbarContextMenu.uc.js to add your own style rules for extension content. Once
-// you have that script installed, you can right-click an addon's toolbar button > Debug Extension >
-// Copy ID. Then, in userContent.css, add a rule like
-// :root[uc-extension-id="example@aminomancer"]{color:red} Keep in mind, the ID is not the same as
-// the URL. That's why this script is necessary in the first place. URLs are random, unique, and
-// per-install. Conversely, an extension's ID is permanent and universal, but potentially not
-// unique, in that two authors could potentially make extensions with the same ID. I haven't seen
-// this before but it's possible, in principle. If you need to, you can find the ID by navigating to
-// about:debugging#/runtime/this-firefox
+// @description    Allows users to share stylesheets for webextensions without
+// needing to edit the URL. This works by creating an actor in every extension
+// browser that sets an attribute on the root element to expose the addon's ID
+// to user stylesheets. This means we can use the addon's ID instead of
+// @-moz-document url(). That is good because addons' URLs are randomly
+// generated upon install, meaning the URLs I specify in
+// resources/in-content/ext-*.css will not be the same as yours, so they will
+// not work for you. You can also use this in combination with my
+// debugExtensionInToolbarContextMenu.uc.js to add your own style rules for
+// extension content. Once you have that script installed, you can right-click
+// an addon's toolbar button > Debug Extension > Copy ID. Then, in
+// userContent.css, add a rule like :root[uc-extension-id="example@aminomancer"]{color:red}
+// Keep in mind, the ID is not the same as the URL. That's why this script is
+// necessary in the first place. URLs are random, unique, and per-install.
+// Conversely, an extension's ID is permanent and universal, but potentially not
+// unique, in that two authors could potentially make extensions with the same
+// ID. I haven't seen this before but it's possible, in principle. If you need
+// to, you can find the ID by navigating to about:debugging#/runtime/this-firefox
 // @license        This Source Code Form is subject to the terms of the Creative Commons Attribution-NonCommercial-ShareAlike International License, v. 4.0. If a copy of the CC BY-NC-SA 4.0 was not distributed with this file, You can obtain one at http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 // @include        main
+// @startup        extensionStylesheetLoader
 // @onlyonce
 // ==/UserScript==
 
@@ -34,21 +38,24 @@ class ExtensionStylesheetLoader {
         tempDir.append(".ExtensionStylesheetLoader");
         let { path } = tempDir;
         await IOUtils.makeDirectory(path, { ignoreExisting: true, createAncestors: false });
-        // hide the temp dir on windows so it doesn't get in the way of user activities or prevent its eventual deletion.
+        // hide the temp dir on windows so it doesn't get in the way of user
+        // activities or prevent its eventual deletion.
         if (AppConstants.platform === "win") {
             if ("setWindowsAttributes" in IOUtils)
                 await IOUtils.setWindowsAttributes(path, { hidden: true });
         }
         this.tempPath = path;
 
-        // create a manifest file that registers a URI for chrome://uc-extensionstylesheetloader/content/
+        // create a manifest file that registers a URI for
+        // chrome://uc-extensionstylesheetloader/content/
         this.manifestFile = await this.createTempFile(`content uc-extensionstylesheetloader ./`, {
             name: "ucsss",
             type: "manifest",
         });
+
         this.childFile = await this.createTempFile(
-            `"use strict";const EXPORTED_SYMBOLS=["ExtensionStylesheetLoaderChild"];const{WebExtensionPolicy}=Cu.getGlobalForObject(Cu);class ExtensionStylesheetLoaderChild extends JSWindowActorChild{handleEvent(e){let policy=WebExtensionPolicy.getByHostname(this.document.location.hostname);if(policy&&policy.id)this.document.documentElement.setAttribute("uc-extension-id",policy.id)}}`,
-            { name: "ExtensionStylesheetLoaderChild", type: "jsm" }
+            `"use strict";const{WebExtensionPolicy}=Cu.getGlobalForObject(Cu);export class ExtensionStylesheetLoaderChild extends JSWindowActorChild{handleEvent(e){let policy=WebExtensionPolicy.getByHostname(this.document.location.hostname);if(policy&&policy.id)this.document.documentElement.setAttribute("uc-extension-id",policy.id)}}`,
+            { name: "ExtensionStylesheetLoaderChild", type: "sys.mjs" }
         );
 
         tempDir.append(this.manifestFile.name);
@@ -56,7 +63,7 @@ class ExtensionStylesheetLoader {
         else return;
         ChromeUtils.registerWindowActor("ExtensionStylesheetLoader", {
             child: {
-                moduleURI: this.childFile.url,
+                esModuleURI: this.childFile.url,
                 events: { DOMDocElementInserted: {} },
             },
             allFrames: true,
@@ -69,8 +76,14 @@ class ExtensionStylesheetLoader {
     /**
      * create a file in the temp folder
      * @param {string} contents (the actual file contents in UTF-8)
-     * @param {object} options (an optional object containing properties path or name. path creates a file at a specific absolute path. name creates a file of that name in the chrome/.ExtensionStylesheetLoader folder. if omitted, it will create chrome/.ExtensionStylesheetLoader/uc-temp)
-     * @returns {object} { name, url } (an object containing the filename and a chrome:// URL leading to the file)
+     * @param {object} options (an optional object containing properties path or
+     *                         name. path creates a file at a specific absolute
+     *                         path. name creates a file of that name in the
+     *                         chrome/.ExtensionStylesheetLoader folder.
+     *                         if omitted, it will create
+     *                         chrome/.ExtensionStylesheetLoader/uc-temp)
+     * @returns {object} (an object containing the filename and
+     *                   a chrome:// URL leading to the file)
      */
     async createTempFile(contents, options = {}) {
         let { path = null, name = "uc-temp", type = "txt" } = options;
@@ -108,4 +121,4 @@ class ExtensionStylesheetLoader {
     }
 }
 
-if (location.href === "chrome://browser/content/browser.xhtml") new ExtensionStylesheetLoader();
+if (window.location.href === AppConstants.BROWSER_CHROME_URL) new ExtensionStylesheetLoader();
--- userChromeJS/moveReloadIntoUrl.uc.js
+++ userChromeJS/moveReloadIntoUrl.uc.js
@@ -4,32 +4,33 @@
 // @compatibility  Firefox 57
 // @author         Ryan, GOLF-AT
 // @include        main
-// @startup        window.moveReloadIntoURL.init();
+// @startup        window.moveReloadIntoURL.init(win);
 // @shutdown       window.moveReloadIntoURL.unload();
 // @homepageURL    https://github.com/benzBrake/FirefoxCustomize
-// @version        1.2.1
+// @version        1.2.2
+// @note           1.2.2 修复 Firefox 103 兼容性
 // @note           1.2 改成可热插拔，兼容夜间模式，图片内置到脚本
 // @note           1.1 20220424 修复，兼容性未知，FF 100 测试通过
 // @note           1.0 20171104
 // @onlyonce
 // ==/UserScript==
-location.href.startsWith('chrome://browser/content/browser.x') && (function () {
+(function () {
     if (window.moveReloadIntoURL) {
-        window.moveReloadIntoURL.destroy();
+        window.moveReloadIntoURL.unload();
         delete window.moveReloadIntoURL;
     }
-
     window.moveReloadIntoURL = {
-        init: function () {
+        init: function (win) {
+            this.doc = doc = win ? win.document : document;
             try {
                 this.sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
-                this.paBtns = document.getElementById("pageActionButton");
-                this.reloadBtn = document.getElementById("reload-button");
-                this.btn = document.getElementById('new-stop-reload-button');
+                this.paBtns = doc.getElementById("pageActionButton");
+                this.reloadBtn = doc.getElementById("reload-button");
+                this.btn = doc.getElementById('new-stop-reload-button');
                 if (!!this.btn || !this.paBtns || !this.reloadBtn) return;
 
-                let btn = document.createXULElement('hbox');
-                let img = document.createXULElement('image');
+                let btn = doc.createXULElement('hbox');
+                let img = doc.createXULElement('image');
                 btn.setAttribute("id", "new-stop-reload-button");
                 btn.setAttribute("class", "urlbar-page-action urlbar-addon-page-action");
                 btn.setAttribute("tooltiptext", Services.locale.appLocaleAsBCP47.includes("zh-") ? '左键：刷新\r\n右键：强制刷新' : 'Left click: refresh page\nRight click: force refresh page')
@@ -49,7 +50,7 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function () {
 
                 }, false);
                 this.paBtns.after(btn);
-                this.btn = document.getElementById('new-stop-reload-button');
+                this.btn = doc.getElementById('new-stop-reload-button');
                 this.reloadBtn.addEventListener('DOMAttrModified', this.reloadBtnAttr);
                 this.reloadBtnAttr();
                 this.reloadBtn.parentNode.hidden = true;
@@ -68,9 +69,10 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function () {
         },
 
         reloadBtnAttr: function (e) {
+            let doc = e ? e.ownerDocument : this.doc;
             btn = window.moveReloadIntoURL.btn;
             if (btn && (!e || e.attrName == 'displaystop')) {
-                var newVal = e ? e.newValue : document.getElementById(
+                var newVal = e ? e.newValue : doc.getElementById(
                     "reload-button").getAttribute('displaystop');
                 if (newVal)
                     btn.style.listStyleImage = "url('data:image/svg+xml;base64,PCEtLSBUaGlzIFNvdXJjZSBDb2RlIEZvcm0gaXMgc3ViamVjdCB0byB0aGUgdGVybXMgb2YgdGhlIE1vemlsbGEgUHVibGljCiAgIC0gTGljZW5zZSwgdi4gMi4wLiBJZiBhIGNvcHkgb2YgdGhlIE1QTCB3YXMgbm90IGRpc3RyaWJ1dGVkIHdpdGggdGhpcwogICAtIGZpbGUsIFlvdSBjYW4gb2J0YWluIG9uZSBhdCBodHRwOi8vbW96aWxsYS5vcmcvTVBMLzIuMC8uIC0tPgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDE2IDE2IiB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIGZpbGw9ImNvbnRleHQtZmlsbCIgZmlsbC1vcGFjaXR5PSJjb250ZXh0LWZpbGwtb3BhY2l0eSI+CiAgPHBhdGggZD0ibTkuMTA4IDcuNzc2IDQuNzA5LTQuNzA5YS42MjYuNjI2IDAgMCAwLS44ODQtLjg4NUw4LjI0NCA2Ljg3MWwtLjQ4OCAwLTQuNjg5LTQuNjg4YS42MjUuNjI1IDAgMSAwLS44ODQuODg1TDYuODcgNy43NTRsMCAuNDkxLTQuNjg3IDQuNjg3YS42MjYuNjI2IDAgMCAwIC44ODQuODg1TDcuNzU0IDkuMTNsLjQ5MSAwIDQuNjg3IDQuNjg3YS42MjcuNjI3IDAgMCAwIC44ODUgMCAuNjI2LjYyNiAwIDAgMCAwLS44ODVMOS4xMDggOC4yMjNsMC0uNDQ3eiIvPgo8L3N2Zz4K')";
@@ -99,5 +101,5 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function () {
         },
     }
 
-    window.moveReloadIntoURL.init();
+    window.moveReloadIntoURL.init(window);
 })();
