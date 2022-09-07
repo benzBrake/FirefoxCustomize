1b6ce23c42d27d50c32820c2e217aaf532151a2f
Ryan Lieu <github-benzBrake@woai.ru>
2022-09-07 15:20:19 +0800

    fix: (JS) movablePanelUIButton.uc.js 新窗口不能定制界面

--- userChromeJS/movablePanelUIButton.uc.js
+++ userChromeJS/movablePanelUIButton.uc.js
@@ -10,11 +10,12 @@
 // @note            2022.08.27 fx 102+
 // @note            2022.07.02 非 xiaoxiaoflood 的 userChromeJS 环境测试可用
 // @note            2022.04.20 修改为可热插拔（不知道非 xiaoxiaoflood 的 userChromeJS 环境是否可用）
+// @onlyonce
 // ==/UserScript==
 (function () {
     const CustomizableUI = globalThis.CustomizableUI || Cu.import("resource:///modules/CustomizableUI.jsm").CustomizableUI;
 
-    if (window.movablePanelUIButton && window.movablePanelUIButton.destroy) {
+    if (window.movablePanelUIButton) {
         window.movablePanelUIButton.destroy();
     }
 
@@ -38,53 +39,79 @@
             `)),
         },
         listener: {
+            windows: windows,
             onCustomizeStart(win) {
-                let { STYLE_DISPLAY: style, sss } = win.movablePanelUIButton;
-                sss.unregisterSheet(style.url, style.type);
+                this.windows(function (doc, win, location) {
+                    win.dispatchEvent(new CustomEvent("MovablePanelUIButtonShow"));
+                })
             },
             onCustomizeEnd(win) {
-                let { STYLE_DISPLAY: style, sss } = win.movablePanelUIButton;
-                sss.loadAndRegisterSheet(style.url, style.type);
+                this.windows(function (doc, win, location) {
+                    win.dispatchEvent(new CustomEvent("MovablePanelUIButtonHide"));
+                })
             }
         },
         init: function () {
             this.sss.loadAndRegisterSheet(this.STYLE_ICON.url, this.STYLE_ICON.type);
             this.sss.loadAndRegisterSheet(this.STYLE_DISPLAY.url, this.STYLE_DISPLAY.type);
             CustomizableUI.addListener(this.listener);
-            if (CustomizableUI.getWidget('movable-PanelUI-button')) {
-                let { node } = CustomizableUI.getWidget('movable-PanelUI-button').forWindow(window);
-                node.addEventListener('mousedown', this);
-                node.addEventListener('keypress', this);
-            } else {
-                CustomizableUI.createWidget({
-                    id: "movable-PanelUI-button",
-                    type: "button",
-                    defaultArea: CustomizableUI.AREA_NAVBAR,
-                    localized: false,
-                    removable: true,
-                    onCreated: node => {
-                        node.addEventListener('mousedown', this);
-                        node.addEventListener('keypress', this);
-                        let pNode = node.ownerDocument.getElementById('PanelUI-menu-button');
-                        ['label', 'tooltiptext'].forEach(attr => node.setAttribute(attr, pNode.getAttribute(attr)));
-                    }
-                });
-            }
+            CustomizableUI.createWidget({
+                id: "movable-PanelUI-button",
+                type: "button",
+                defaultArea: CustomizableUI.AREA_NAVBAR,
+                localized: false,
+                removable: true,
+                onCreated: node => {
+                    node.addEventListener('mousedown', this);
+                    node.addEventListener('keypress', this);
+                    let pNode = node.ownerDocument.getElementById('PanelUI-menu-button');
+                    ['label', 'tooltiptext'].forEach(attr => node.setAttribute(attr, pNode.getAttribute(attr)));
+                }
+            });
+            window.addEventListener('MovablePanelUIButtonShow', this);
+            window.addEventListener('MovablePanelUIButtonHide', this);
         },
         handleEvent: function (event) {
             if (event.type === "mousedown" && event.button !== 0) return;
-            let { target: node } = event;
-            let { ownerDocument: document } = node;
-            const { PanelUI } = document.defaultView;
-            PanelUI.menuButton = node;
-            PanelUI.show();
+            switch (event.type) {
+                case 'mousedown':
+                case 'keypress':
+                    let { target: node } = event;
+                    let { ownerDocument: document } = node;
+                    const { PanelUI } = document.defaultView;
+                    PanelUI.menuButton = node;
+                    PanelUI.show();
+                    break;
+                case 'MovablePanelUIButtonShow':
+                    this.sss.unregisterSheet(this.STYLE_DISPLAY.url, this.STYLE_DISPLAY.type);
+                    break;
+                case 'MovablePanelUIButtonHide':
+                    this.sss.loadAndRegisterSheet(this.STYLE_DISPLAY.url, this.STYLE_DISPLAY.type);
+                    break;
+            }
+
         },
         unload: function () {
             this.sss.unregisterSheet(this.STYLE_ICON.url, this.STYLE_ICON.type);
             this.sss.unregisterSheet(this.STYLE_DISPLAY.url, this.STYLE_DISPLAY.type);
             CustomizableUI.destroyWidget("movable-PanelUI-button");
             document.defaultView.PanelUI.menuButton = document.getElementById('PanelUI-button');
+            window.removeEventListener('MovablePanelUIButtonShow', this);
+            window.removeEventListener('MovablePanelUIButtonHide', this);
+            delete window.movablePanelUIButton;
         }
     }
     window.movablePanelUIButton.init();
+
+    function windows(fun) {
+        let windows = Services.wm.getEnumerator('navigator:browser');
+        while (windows.hasMoreElements()) {
+            let win = windows.getNext();
+            if (!win._uc)
+                continue;
+            let { document, location } = win;
+            if (fun(document, win, location))
+                break;
+        }
+    }
 })();
