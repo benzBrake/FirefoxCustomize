4520e2c55edccfacf1d8a13610c24fde3eb99b89
Ryan Lieu <github-benzBrake@woai.ru>
2022-09-10 00:18:55 +0800

    fix: (JS) BMMultiColumn.uc.js 边距问题 feat: (JS) BMMultiColumn.uc.js 支持书签工具栏溢出菜单

--- userChromeJS/BMMultiColumn.uc.js
+++ userChromeJS/BMMultiColumn.uc.js
@@ -4,8 +4,10 @@
 // @author          Ryan, ding
 // @include         main
 // @charset         UTF-8
-// @version         2022.08.08
+// @version         2022.09.10
 // @shutdown        window.BMMultiColumn.destroy();
+// @homepageURL     https://github.com/benzBrake/FirefoxCustomize/blob/master/userChromeJS
+// @note            修复边距问题，支持书签工具栏溢出菜单
 // @note            修复有时候无法启用
 // @note            适配Firefox57+
 // ==/UserScript==
@@ -32,6 +34,11 @@
             if (this.BUTTON) {
                 this.BUTTON.addEventListener('click', this, { once: true });
             }
+            this.TOOLBAR_CHERVON = $("PlacesChevronPopup");
+            if (this.TOOLBAR_CHERVON) {
+                this.TOOLBAR_CHERVON.addEventListener('popupshowing', this, false);
+                this.TOOLBAR_CHERVON.addEventListener('popuphiding', this, false);
+            }
         },
         destroy: function () {
             if (this.TOOLBAR) {
@@ -48,6 +55,10 @@
                         }
                     }
                 });
+            }
+            if (this.TOOLBAR_CHERVON) {
+                this.TOOLBAR_CHERVON.removeEventListener('popupshowing', this, false);
+                this.TOOLBAR_CHERVON.removeEventListener('popuphiding', this, false);
             }
         },
         handleEvent: function (event) {
@@ -83,14 +94,14 @@
                                 menuitem = menuitem.previousSibling;
                             }
                             if (!(scrollbox.width == box.scrollWidth)) {
-                                var leftFix = parseInt(getComputedStyle(menupopup).paddingInlineStart);
-                                var rightFix = parseInt(getComputedStyle(menupopup).paddingInlineEnd);
                                 var firstItem = menupopup.querySelector('menuitem');
-                                var columns = 0;
+                                // var columns = 0;
+                                var offset = 0;
                                 if (firstItem) {
-                                    columns = parseInt(box.scrollWidth / parseInt(getComputedStyle(firstItem).width))
+                                    // columns = parseInt(box.scrollWidth / parseInt(getComputedStyle(firstItem).width))
+                                    offset = offsetX(firstItem) / 2;
                                 }
-                                scrollbox.width = box.scrollWidth + (columns || 1) * (leftFix || 0 + rightFix || 0);
+                                scrollbox.width = box.scrollWidth + offset;
                             }
                         }
                     }
@@ -119,6 +130,7 @@
                             menuitem = menuitem.previousSibling;
                         }
 
+                        // 注释这一行防抖动
                         scrollbox.width = "";
                     }
                     break;
@@ -149,6 +161,13 @@
         let doc = aDoc || document;
         if (id.startsWith('#')) id = id.substring(1, id.length);
         return doc.getElementById(id);
+    }
+
+    function offsetX(elm) {
+        var elmMargin, elmPadding;
+        elmMargin = parseInt(document.defaultView.getComputedStyle(elm, '').getPropertyValue('margin-inline-start')) + parseInt(document.defaultView.getComputedStyle(elm, '').getPropertyValue('margin-right'));
+        elmPadding = parseInt(document.defaultView.getComputedStyle(elm, '').getPropertyValue('padding-inline-end')) + parseInt(document.defaultView.getComputedStyle(elm, '').getPropertyValue('padding-inline-end'));
+        return elmMargin + elmPadding;
     }
 
     if (gBrowserInit.delayedStartupFinished) window.BMMultiColumn.init();
