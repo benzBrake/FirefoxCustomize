7122362f679c63c258a6a1df68e21f5457f82a4a
Ryan Lieu <github-benzBrake@woai.ru>
2022-10-01 16:16:58 +0800

    feat: (JS) 新增 ucf_drag_ModR.uc.js

---
+++ userChromeJS/ucf_drag_ModR.uc.js
@@ -0,0 +1,444 @@
+// ==UserScript==
+// @name            ucf_drag_ModR.uc.js
+// @description     鼠标拖拽 Drag & Go
+// @note            来自于 Mozilla-Russia 论坛，Ryan 修改自用
+// @author          Ryan, Dumby
+// @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @referenceURL    https://forum.mozilla-russia.org/viewtopic.php?pid=797234#p797234
+// @onlyonce
+// ==/UserScript==
+
+(async win => ({
+    debug: false,
+    link: {
+        U: {
+            name: "打开链接（新标签，前台）", cmd(val) {
+                win.openUILinkIn(val, "tab", this.opts);
+            }
+        },
+        R: {
+            name: "打开链接（新标签，后台）", cmd(val) {
+                win.openUILinkIn(val, "tabshifted", this.opts);
+            }
+        },
+        RD: {
+            name: "另存链接", cmd(val) {
+                this.saveAs(val);
+            }
+        },
+        L: {
+            name: "复制链接", cmd(val) {
+                this.copyString(val);
+            }
+        },
+        D: {
+            name: "打开链接（当前标签）", cmd(val) {
+                win.openUILinkIn(val, "current", this.opts);
+            }
+        },
+        LD: {
+            name: "以站搜站（新标签，前台）", cmd(val) {
+                if (!val) return;
+                var TERM = "https://www.similarsites.com/site/" + new URL(val).hostname.replace(/^www./, '');
+                if (val)
+                    win.openUILinkIn(TERM, "tab", this.opts);
+            }
+        },
+        "LD-Shift": {
+            name: "网页历史（新标签，前台）", cmd(val) {
+                if (!val) return;
+                var TERM = "https://web.archive.org/web/*/" + new URL(val).hostname.replace(/^www./, '');
+                if (val)
+                    win.openUILinkIn(TERM, "tab", this.opts);
+            }
+        },
+    },
+    text: {
+        U: {
+            name: "搜索文本（新标签，前台）", cmd(val) {
+                this.searchWithEngine(val, "tab", "@default");
+            }
+        },
+        "U-Shift": {
+            name: "搜索文本（新标签，后台）", cmd(val) {
+                this.searchWithEngine(val, "tabshifted", "@default");
+            }
+        },
+        R: {
+            name: "百度搜索（新标签，前台）", cmd(val) {
+                this.searchWithEngine(val, 'tab', '百度');
+            }
+        },
+        "R-Shift": {
+            name: "百度搜索（新标签，后台）", cmd(val) {
+                this.searchWithEngine(val, 'tabshifted', '百度');
+            }
+        },
+        RD: {
+            name: "另存文本", cmd(val) {
+                this.saveText(val);
+            }
+        },
+        D: {
+            name: "站内搜索（当前标签）", cmd(val, event) {
+                var currentPageUrl = event.originalTarget._urlMetaData['url'];
+                var TERM = "site:" + new URL(currentPageUrl).hostname.replace(/^www./, '') + " " + val;
+                if (val)
+                    this.searchWithEngine(TERM, 'current', '@default');
+            }
+        },
+        "D-Shift": {
+            name: "站内搜索（新标签，前台）", cmd(val, event) {
+                var currentPageUrl = event.originalTarget._urlMetaData['url'];
+                var TERM = "site:" + new URL(currentPageUrl).hostname.replace(/^www./, '') + " " + val;
+                if (val)
+                    this.searchWithEngine(TERM, 'tab', '@default');
+            }
+        },
+        L: {
+            name: "复制文本", cmd(val) {
+                this.copyString(val);
+            }
+        },
+        LD: {
+            name: "剑桥词典（新标签，前台）", cmd(val) {
+                var TERM = "https://dictionary.cambridge.org/zhs/%E8%AF%8D%E5%85%B8/%E8%8B%B1%E8%AF%AD/" + val;
+                if (val)
+                    win.openUILinkIn(TERM, "tab", this.opts);
+            }
+        },
+        "LD-Shift": {
+            name: "剑桥词典（新标签，后台）", cmd(val) {
+                var TERM = "https://dictionary.cambridge.org/zhs/%E8%AF%8D%E5%85%B8/%E8%8B%B1%E8%AF%AD/" + val;
+                if (val)
+                    win.openUILinkIn(TERM, "tabshifted", this.opts);
+            }
+        }
+    },
+    image: {
+        U: {
+            name: "打开图像（新标签，前台）", cmd() {
+                win.openUILinkIn(this.val, "tab", this.opts);
+            }
+        },
+        R: {
+            name: "打开图像（新标签，后台）", cmd() {
+                win.openUILinkIn(this.val, "tabshifted", this.opts);
+            }
+        },
+        RD: {
+            name: "另存图像", cmd(val) {
+                this.saveAs(val);
+            }
+        },
+        L: {
+            name: "复制图片链接", cmd(val) {
+                this.copyString(val);
+            }
+        },
+        LD: {
+            name: "谷歌搜图（新标签，前台）", cmd(val) {
+                var TERM = "https://www.google.com/searchbyimage?image_url=" + val;
+                if (val)
+                    win.openUILinkIn(TERM, "tab", this.opts);
+            }
+        },
+        "LD-Shift": {
+            name: "Yandex搜图（新标签，前台）", cmd(val) {
+                var TERM = "https://yandex.com/images/search?source=collections&rpt=imageview&url=" + val;
+                if (val)
+                    win.openUILinkIn(TERM, "tabshifted", this.opts);
+            }
+        },
+    },
+    searchWithEngine(val, where, engine, addToHistory) {
+        val || (val = this.val);
+        var engine = this.getEngineByName(engine);
+        var submission = engine.getSubmission(val, null);
+        win.openUILinkIn(submission.uri.spec, where, { postData: submission.postData, ...this.opts });
+        if (addToHistory) {
+            this.updateSearchbarHistory(val);
+        }
+    },
+    getEngineByName(aEngineName) {
+        const UI = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
+            createInstance(Ci.nsIScriptableUnicodeConverter);
+        UI.charset = "UTF-8";
+        const nsIBSS = Ci.nsIBrowserSearchService || Ci.nsISearchService;
+        const searchService = Cc["@mozilla.org/browser/search-service;1"].getService(nsIBSS);
+        if (aEngineName.toUpperCase() == "CURRENT") {
+            var searchbar = this.searchbar;
+            if (searchbar) return searchbar.currentEngine;
+        } else {
+            try {
+                aEngineName = UI.ConvertToUnicode(aEngineName)
+            } catch (e) { }
+            var engine = searchService.getEngineByName(aEngineName);
+            if (engine) return engine;
+        }
+        //Default
+        return searchService.defaultEngine;
+    },
+    copyToSearchBar(searchText) {
+        var searchbar = this.searchbar;
+        if (!searchbar)
+            return;
+        searchbar.value = searchText;
+    },
+    updateSearchbarHistory(searchText) {
+        this.copyToSearchBar(searchText);
+
+        //var event = document.createEvent("UIEvents");
+        //event.initUIEvent("input", true, true, window, 0);
+        var searchbar = this.searchbar;
+        //searchbar.dispatchEvent(event);
+        if (typeof searchbar.FormHistory == "object") {
+            if (searchText && !win.PrivateBrowsingUtils.isWindowPrivate(window)) {
+                searchbar.FormHistory.update({
+                    op: "bump",
+                    fieldname: searchbar._textbox.getAttribute("autocompletesearchparam"),
+                    value: searchText
+                }, {
+                    handleError: function (aError) {
+                        Components.utils.reportError("Saving search to form history failed: " + aError.message);
+                    }
+                });
+            }
+        } else {
+            if (searchText) {
+                searchbar._textbox._formHistSvc
+                    .addEntry(searchbar._textbox.getAttribute("autocompletesearchparam"),
+                        searchText);
+            }
+        }
+    },
+    copyString(text) {
+        const cs = Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper);
+        cs.copyString(text);
+    },
+    saveAs(aURL, aFileName, aReferrer, aSourceDocument, aContentType, aContentDisposition) {
+        const { gBrowser, PrivateBrowsingUtils, internalSave, saveImageURL, saveURL } = win;
+        let createContentPrincipal = Services.scriptSecurityManager.createContentPrincipal || Services.scriptSecurityManager.createCodebasePrincipal;
+        let aPrincipal = createContentPrincipal(Services.io.newURI(aURL), {});
+        let isPrivate = PrivateBrowsingUtils.isBrowserPrivate(gBrowser.selectedBrowser);
+        const firefoxVer = parseFloat(Services.appinfo.version);
+        const imageLinkRegExp = /(.+)\.(png|jpg|jpeg|gif|bmp)$/i;
+        if (aReferrer instanceof HTMLDocument) {
+            aReferrer = aReferrer.documentURIObject;
+        } else if (typeof aReferrer == 'string') {
+            aReferrer = Services.io.newURI(aReferrer);
+        }
+        if (firefoxVer >= 70) {
+            let referrerInfo = Cc["@mozilla.org/referrer-info;1"].createInstance(Ci.nsIReferrerInfo);
+            referrerInfo.init(
+                Ci.nsIHttpChannel.REFERRER_POLICY_NO_REFERRER_WHEN_DOWNGRADE,
+                true,
+                aReferrer
+            );
+            aReferrer = referrerInfo;
+        }
+        if (imageLinkRegExp.test(aURL) || /^image\//i.test(aContentType)) {
+            if (firefoxVer >= 102.3) {
+                let cookieJarSettings = gBrowser.selectedBrowser.cookieJarSettings;
+                if (/^data:/.test(aURL)) {
+                    internalSave(aURL, null, null, "index.png", aContentDisposition, aContentType, true, null, null, aReferrer, cookieJarSettings, aSourceDocument, false, null, isPrivate, aPrincipal);
+                } else {
+                    internalSave(aURL, null, null, null, aContentDisposition, aContentType, false, null, null, aReferrer, cookieJarSettings, aSourceDocument, false, null, isPrivate, aPrincipal);
+                }
+            } else if (firefoxVer >= 84) {
+                let cookieJarSettings = gBrowser.selectedBrowser.cookieJarSettings;
+                if (/^data:/.test(aURL)) {
+                    internalSave(aURL, null, "index.png", aContentDisposition, aContentType, true, null, null, aReferrer, cookieJarSettings, aSourceDocument, false, null, isPrivate, aPrincipal);
+                } else {
+                    internalSave(aURL, null, null, aContentDisposition, aContentType, false, null, null, aReferrer, cookieJarSettings, aSourceDocument, false, null, isPrivate, aPrincipal);
+                }
+            } else if (firefoxVer >= 77) {
+                if (/^data:/.test(aURL)) {
+                    internalSave(aURL, null, "index.png", aContentDisposition, aContentType, true, null, null, aReferrer, aSourceDocument, false, null, isPrivate, aPrincipal);
+                } else {
+                    internalSave(aURL, null, null, aContentDisposition, aContentType, false, null, null, aReferrer, aSourceDocument, false, null, isPrivate, aPrincipal);
+                }
+            } else {
+                if (/^data:/.test(aURL)) {
+                    saveImageURL(aURL, "index.png", null, true, false, aReferrer, aSourceDocument, aContentType, aContentDisposition, isPrivate, aPrincipal);
+                } else {
+                    saveImageURL(aURL, null, null, false, false, aReferrer, aSourceDocument, aContentType, aContentDisposition, isPrivate, aPrincipal);
+                }
+            }
+        } else {
+            if (firefoxVer >= 102.3) {
+                let cookieJarSettings = gBrowser.selectedBrowser.cookieJarSettings;
+                saveURL(aURL, null, aFileName, null, true, false, aReferrer, cookieJarSettings, aSourceDocument, isPrivate, aPrincipal);
+            } else if (firefoxVer >= 84) {
+                let cookieJarSettings = gBrowser.selectedBrowser.cookieJarSettings;
+                saveURL(aURL, aFileName, null, true, false, aReferrer, cookieJarSettings, aSourceDocument, isPrivate, aPrincipal);
+            } else {
+                saveURL(aURL, aFileName, null, true, false, aReferrer, aSourceDocument, isPrivate, aPrincipal);
+            }
+        }
+    },
+    saveText: async function saveText(text) {
+        const { Cc, Ci, gBrowser } = win;;
+        const { nsIFilePicker } = Ci;
+        var fp = Cc['@mozilla.org/filepicker;1'].createInstance(nsIFilePicker);
+        fp.init(win, "Select a File", Ci.nsIFilePicker.modeSave);
+        fp.appendFilters(nsIFilePicker.filterText);
+        fp.defaultString = gBrowser.contentTitle.replace(/\s-\s.*/i, "").replace(/_[^\[\]【】]+$/, "") + '.txt';
+        switch (await new Promise(resolve => { fp.open(rv => { resolve(rv); }); })) {
+            case (nsIFilePicker.returnOK):
+            case (nsIFilePicker.returnReplace):
+                file = fp.file;
+                break;
+            case (nsIFilePicker.returnCancel):
+            default:
+                return null;
+        }
+        var strm = Cc["@mozilla.org/network/file-output-stream;1"]
+            .createInstance(Ci.nsIFileOutputStream);
+        var convert = Cc['@mozilla.org/intl/scriptableunicodeconverter']
+            .getService(Ci.nsIScriptableUnicodeConverter);
+        convert.charset = "UTF-8";
+        ext = convert.ConvertFromUnicode(text);
+        try {
+            strm.init(file, 0x02 | 0x08 | 0x20, parseInt(664, 8), 0); // write, create, truncate
+            strm.write(text, text.length);
+            strm.flush();
+        } catch (ex) {
+            alert('failed:\n' + ex);
+            file = null;
+        }
+        strm.close();
+
+        return file;
+    },
+    printDataTransferTypes: function (e) {
+        var dt = e.dataTransfer;
+        console.info("print dataTransfer type:");
+        var types = dt.types;
+        for (var i = 0; i < types.length; i += 1) {
+            console.info(types[i] + ": " + dt.getData(types[i]));
+        }
+    },
+    opts: {
+        //relatedToCurrent: true,
+        triggeringPrincipal: Cu.getObjectPrincipal(this),
+        get userContextId() {
+            return parseInt(win.gBrowser.selectedBrowser.getAttribute("usercontextid"));
+        },
+        get private() {
+            return win.PrivateBrowsingUtils.isWindowPrivate(win);
+        }
+    },
+    dragstart(e) {
+        win = e.view.windowRoot.ownerGlobal;
+        //if (!win.gBrowser.currentURI.spec.startsWith("http")) return;
+        if (!e.dataTransfer.mozItemCount || !win.gBrowser.selectedBrowser.matches(":hover"))
+            return;
+
+        if (this.debug)
+            this.printDataTransferTypes(e);
+
+        var dt = e.dataTransfer;
+        this.type = this.link;
+        this.dir = this.val = "";
+
+        var url = dt.getData("text/x-moz-url-data");
+
+        if (url) {
+            this.val = url;
+            if (this.imageLinkRe.test(url)) {
+                this.type = this.image;
+            } else {
+                var promiseUrl = dt.getData("application/x-moz-file-promise-url");
+                var dragHTML = dt.getData("text/html");
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(dragHTML, "text/html");
+                var onImage = doc.getRootNode().body?.firstElementChild?.tagName == "IMG" || doc.getRootNode().body?.firstElementChild.querySelectorAll("img").length;
+                if (onImage && e.ctrlKey) {
+                    // force to image type when ctrlKey is pressed
+                    this.type = this.image;
+                    this.val = promiseUrl;
+                }
+            }
+        } else {
+            var txt = dt.getData("text/plain");
+            if (txt) {
+                this.val = txt;
+                if (!this.textLinkRe.test(txt)) this.type = this.text;
+                if (this.imageLinkRe.test(txt)) this.type = this.image;
+            }
+            else return;
+        }
+        this.x = e.screenX; this.y = e.screenY;
+        this.drag(true);
+    },
+    drag(init) {
+        var meth = `${init ? "add" : "remove"}EventListener`;
+        for (var type of this.events) win[meth](type, this, true);
+        init || win.StatusPanel.panel.setAttribute("inactive", true);
+    },
+    events: ["dragover", "drop", "dragend"],
+    dragover(e) {
+        var { x, y } = this, cx = e.screenX, cy = e.screenY;
+        var dx = cx - x, ax = Math.abs(dx), dy = cy - y, ay = Math.abs(dy);
+        if (ax < 10 && ay < 10) return;
+
+        this.x = cx; this.y = cy;
+        var dir = ax > ay ? dx > 0 ? "R" : "L" : dy > 0 ? "D" : "U";
+        if (this.dir.endsWith(dir)) return;
+
+        dir = this.dir += dir;
+        var obj;
+        if (e.shiftKey) {
+            obj = this.type[dir + "-Shift"];
+        } else {
+            obj = this.type[dir];
+        }
+
+        var txt = `${obj ? "鼠标" : "未知"
+            }手势: ${dir + (obj ? "  " + obj.name : "")}`;
+
+        win.StatusPanel._labelElement.value = txt;
+        win.StatusPanel.panel.removeAttribute("inactive");
+    },
+    dragend(e) {
+        var dt = e.dataTransfer;
+        this.drag();
+        var obj;
+        if (e.shiftKey) {
+            obj = this.type[this.dir + "-Shift"];
+        } else {
+            obj = this.type[this.dir];
+        }
+        if (!obj || dt.mozUserCancelled) return;
+
+        var x = e.screenX, y = e.screenY;
+        var wx = win.mozInnerScreenX, wy = win.mozInnerScreenY;
+        x > wx && y > wy && x < wx + win.innerWidth && y < wy + win.innerHeight
+            && obj.cmd.call(this, this.val, e);
+    },
+    textLinkRe: /^([a-z]+:\/\/)?([a-z]([a-z0-9\-]*\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-z][a-z0-9_]*)?$|^custombutton:\/\/\S+$/,
+    imageLinkRe: /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg|avif|webp))/,
+    observe(w) {
+        this.drop = () => this.drag();
+        this.handleEvent = e => this[e.type](e);
+        var unload = e => {
+            var w = e.target.ownerGlobal;
+            w.gBrowser.tabpanels.removeEventListener("dragstart", this, true);
+            if (w == win) win = null;
+        }
+        (this.observe = w => {
+            //if (!w.toolbar.visible) return;
+            w.gBrowser.tabpanels.addEventListener("dragstart", this, true);
+            w.addEventListener("unload", unload, { once: true });
+        })(w);
+    },
+    init(topic, self) {
+        delete this.init;
+        Services.obs.addObserver(self = this, topic);
+        Services.obs.addObserver(function quit(s, t) {
+            Services.obs.removeObserver(self, topic);
+            Services.obs.removeObserver(quit, t);
+        }, "quit-application-granted");
+    }
+}).init("browser-delayed-startup-finished"))();
