6987302b7db2f37da90d9f059435fbbc4fcb9790
Ryan Lieu <github-benzBrake@woai.ru>
2022-11-13 11:42:34 +0800

    fix: (JS) AutoCopySelectionText.uc.js 修复移动鼠标即复制

--- userChromeJS/AutoCopySelectionText.uc.js
+++ userChromeJS/AutoCopySelectionText.uc.js
@@ -2,7 +2,7 @@
 // @name            AutoCopySelectionText.uc.js
 // @description     自动复制选中文本（ScrLk 亮起时不复制）
 // @author          Ryan
-// @version         2022.10.11
+// @version         2022.11.13
 // @compatibility   Firefox 57
 // @charset         UTF-8
 // @system          windows
@@ -10,6 +10,7 @@
 // @include         main
 // @shutdown        window.AutoCopySelectionText.destroy();
 // @homepageURL     https://github.com/benzBrake/FirefoxCustomize/tree/master/userChromeJS
+// @version         2022.11.13 修复移动鼠标即复制
 // @version         2022.10.11 增加文本框开关
 // @version         2022.07.28 网页支持文本框
 // @version         2022.07.18 支持长按延时
@@ -34,13 +35,18 @@
 
                 // implement read from about:config preferences in future
                 var WAIT_TIME = 0; // Change it to any number as you want
-                var TRIM_SELECTION = false; // remove spaces before and after the string
                 var DISABLE_IN_TEXTBOX = true; // disable auto copy when focus on textboxes
 
                 // Do not modify below ------------------------------------------
                 var LONG_PRESS = false;
                 var TIMEOUT_ID = null;
+
+                var isOnTextInput = function (elem) {
+                    return elem instanceof HTMLTextAreaElement ||
+                        (elem instanceof HTMLInputElement && elem.mozIsTextField(true));
+                };
                 function handleEvent(event) {
+                    let win = event.target.ownerGlobal;
                     if (event.button !== 0) return; // only trigger when left button up
                     if (TIMEOUT_ID)
                         content.clearTimeout(TIMEOUT_ID);
@@ -50,10 +56,18 @@
                             TIMEOUT_ID = content.setTimeout(function () {
                                 LONG_PRESS = true;
                             }, WAIT_TIME);
+                            break;
                         case 'mouseup':
                             // copy text on mouse button up
                             if (LONG_PRESS) {
-                                let data = { text: BrowserOrSelectionUtils.getSelectionDetails(event.target.ownerGlobal).fullText }
+                                let focusedElement =
+                                    Services.focus.focusedElement ||
+                                    (event.originalTarget.ownerDocument ? event.originalTarget.ownerDocument.activeElement : null);
+
+                                if (focusedElement && DISABLE_IN_TEXTBOX && isOnTextInput(focusedElement)) return;
+                                let selctionDetails = BrowserOrSelectionUtils.getSelectionDetails(win);
+                                if (!selctionDetails.fullText) return;
+                                let data = { text: selctionDetails.fullText }
                                 sendSyncMessage("acst_selectionData", data);
                             }
                             break;
@@ -80,12 +94,12 @@
             window.messageManager.loadFrameScript(frameScriptURI, true);
             window.messageManager.addMessageListener("acst_selectionData", this);
         }
-        receiveMessage(message) {
-            switch (message.name) {
+        receiveMessage({ name, data }) {
+            switch (name) {
                 case 'acst_selectionData':
                     if (this.getKeyState(0x91)) return;
-                    if (message.data.text)
-                        Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(message.data.text);
+                    if (data.text)
+                        Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(data.text);
                     break;
             }
         }
