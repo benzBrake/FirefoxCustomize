cd334f2566f7f9207b32fe9f48113a7782a292b2
Ryan Lieu <github-benzBrake@woai.ru>
2024-03-13 15:35:18 +0800

    feat: (JS) addMenuPlus.uc.js,addMenuPlus_fx70.uc.js dom 属性 image 转换为css 属性 list-style-image

--- userChromeJS/addMenuPlus/addMenuPlus.uc.js
+++ userChromeJS/addMenuPlus/addMenuPlus.uc.js
@@ -15,7 +15,7 @@
 // @oohomepageURL  https://github.com/Griever/userChromeJS/tree/master/addMenu
 // @reviewURL      http://bbs.kafan.cn/thread-1554431-1-1.html
 // @downloadURL    https://github.com/ywzhaiqi/userChromeJS/raw/master/addmenuPlus/addMenuPlus.uc.js
-// @note           0.1.5 fix openUILinkIn was removed, Bug 1820534 - Move front-end to modern flexbox，修复 about:error 页面获取的地址不对, Bug 1815439 - Remove useless loadURI wrapper from browser.js, 扩展 %FAVICON% %FAVICON_BASE64% 的应用范围, condition 支持多个条件，支持 %sl 选中文本或者链接文本，openCommand 函数增加额外参数，Bug 1870644 - Provide a single function for obtaining icon URLs from search engines
+// @note           0.1.5 fix openUILinkIn was removed, Bug 1820534 - Move front-end to modern flexbox，修复 about:error 页面获取的地址不对, Bug 1815439 - Remove useless loadURI wrapper from browser.js, 扩展 %FAVICON% %FAVICON_BASE64% 的应用范围, condition 支持多个条件，支持 %sl 选中文本或者链接文本，openCommand 函数增加额外参数，Bug 1870644 - Provide a single function for obtaining icon URLs from search engines，dom 属性 image 转换为css 属性 list-style-image
 // @note           0.1.4 onshowing/onshowinglabel 在所有右键菜单生效, 更换语言读取方式，修正 Linux 下 exec 的兼容性
 // @note           0.1.3 修正 Firefox 78 (?应该是吧) openUILinkIn 参数变更；Firefox 92 getURLSpecFromFile 废止，切换到 getURLSpecFromActualFile；添加到文件菜单的 app/appmenu 菜单自动移动到汉堡菜单, 修复 keyword 调用搜索引擎失效的问题，没有 label 并使用 keyword 调用搜索引擎时设置 label 为搜素引擎名称；增加 onshowinglabel 属性，增加本地化属性 data-l10n-href 以及 data-l10n-id；修正右键未显示时无法获取选中文本，增加菜单类型 nav （navigator-toolbox的右键菜单），兼容 textLink_e10s.uc.js，增加移动的菜单无需重启浏览器即可还原，增加 identity-box 右键菜单, getSelectionText 完美修复，支持内置页面，修复右键菜单获取选中文本不完整
 // @note           0.1.2 增加多语言；修复 %I %IMAGE_URL% %IMAGE_BASE64% 转换为空白字符串；GroupMenu 增加 onshowing 事件
@@ -1215,6 +1215,12 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function (css
                 menuitem.setAttribute('tooltiptext', obj.label);
             }
 
+            // 转换 image 属性为 style 属性
+            if (menuitem.hasAttribute("image") && /-iconic/.test(menuitem.className)) {
+                menuitem.style.listStyleImage = "url(" + menuitem.getAttribute("image") + ")";
+                menuitem.removeAttribute("image");
+            }
+
             // oncommand, command はここで終了
             if (obj.oncommand || obj.command)
                 return menuitem;
@@ -1344,8 +1350,8 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function (css
                 e.classList.remove('addMenuHide');
             });
         },
-
         setIcon: function (menu, obj) {
+
             if (menu.hasAttribute("src") || menu.hasAttribute("image") || menu.hasAttribute("icon"))
                 return;
 
@@ -1362,9 +1368,9 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function (css
                 } else {
                     if (aFile.isFile()) {
                         let fileURL = this.getURLSpecFromFile(aFile);
-                        menu.setAttribute("image", "moz-icon://" + fileURL + "?size=16");
+                        menu.style.listStyleImage = "url(moz-icon://" + fileURL + "?size=16)";
                     } else {
-                        menu.setAttribute("image", "chrome://global/skin/icons/folder.svg");
+                        menu.style.listStyleImage = "url(chrome://global/skin/icons/folder.svg)";
                     }
                 }
                 return;
@@ -1376,11 +1382,11 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function (css
                     if (isPromise(engine)) {
                         engine.then(function (engine) {
                             let iconURL = getIconURL(engine)
-                            if (iconURL) menu.setAttribute("image", iconURL);
+                            if (iconURL) menu.style.listStyleImage = `url("${iconURL}")`;
                         });
                     } else if (engine.iconURI) {
                         // 非异步只能从 iconURI 获取
-                        menu.setAttribute("image", engine.iconURI.spec);
+                        menu.style.listStyleImage = `url("${engine.iconURI.spec})"`;
                     }
                     return;
                 }
@@ -1407,13 +1413,15 @@ location.href.startsWith('chrome://browser/content/browser.x') && (function (css
                     onComplete: function (aURI, aDataLen, aData, aMimeType) {
                         try {
                             // javascript: URI の host にアクセスするとエラー
-                            menu.setAttribute("image", aURI && aURI.spec ?
+                            let iconURL = aURI && aURI.spec ?
                                 "moz-anno:favicon:" + aURI.spec :
-                                "moz-anno:favicon:" + uri.scheme + "://" + uri.host + "/favicon.ico");
+                                "moz-anno:favicon:" + uri.scheme + "://" + uri.host + "/favicon.ico";
+                            menu.style.listStyleImage = `url(${iconURL})`;
                         } catch (e) { }
                     }
                 });
             }
+
             PlacesUtils.keywords.fetch(obj.keyword || '').then(entry => {
                 let url;
                 if (entry) {
